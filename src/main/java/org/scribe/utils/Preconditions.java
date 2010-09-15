b2147de2e52d1128ad45349b68b639b693cde405
Pablo Fernandez <fernandezpablo85@gmail.com>
2010-09-15 10:57:14 -0300

    Removed last isEmpty for < 1.6 compatibility

--- src/main/java/org/scribe/utils/Preconditions.java
+++ src/main/java/org/scribe/utils/Preconditions.java
@@ -38,7 +38,7 @@ public class Preconditions
   
   private static void check(boolean requirements, String error)
   {
-    String message = (error == null || error.trim().isEmpty()) ? DEFAULT_MESSAGE : error;
+    String message = (error == null || error.trim().length() <= 0) ? DEFAULT_MESSAGE : error;
     if (!requirements)
     {
       throw new IllegalArgumentException(message);
