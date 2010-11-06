9337ac01d9d7fac71d909a48553b38faff1c7253
Pablo Fernandez <fernandezpablo85@gmail.com>
2010-11-06 20:03:13 -0300

    Fixed EvernoteApi to use GET

--- src/main/java/org/scribe/builder/api/EvernoteApi.java
+++ src/main/java/org/scribe/builder/api/EvernoteApi.java
@@ -1,7 +1,14 @@
 package org.scribe.builder.api;
 
+import org.scribe.model.Verb;
+
 public class EvernoteApi extends DefaultApi10a
 {
+  @Override
+  protected Verb getRequestTokenVerb()
+  {
+    return Verb.GET;
+  }
 
 	@Override
 	public String getRequestTokenEndpoint()
@@ -10,9 +17,14 @@ public class EvernoteApi extends DefaultApi10a
 	}
 
 	@Override
-	public String getAccessTokenEndpoint()
-  {
-		return "https://www.evernote.com/oauth";
+	protected Verb getAccessTokenVerb()
+	{
+	  return Verb.GET;
 	}
 
+	@Override
+	public String getAccessTokenEndpoint()
+	{
+		return "https://www.evernote.com/oauth";
+	}
 }
