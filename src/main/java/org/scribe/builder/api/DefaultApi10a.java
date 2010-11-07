577c18c7524f1731521cf6f38845d4ecf10c5ec6
Pablo Fernandez <fernandezpablo85@gmail.com>
2010-11-07 18:35:59 -0300

    corrected javadocs to reflect changes

--- src/main/java/org/scribe/builder/api/DefaultApi10a.java
+++ src/main/java/org/scribe/builder/api/DefaultApi10a.java
@@ -8,51 +8,96 @@ import org.scribe.services.*;
 /**
  * Default implementation of the OAuth protocol, version 1.0a
  * 
- * This class is meant to be extended by concrete implementations of the API.
+ * This class is meant to be extended by concrete implementations of the API,
+ * providing the endpoints and endpoint-http-verbs.
+ * 
  * If your Api adheres to the 1.0a protocol correctly, you just need to extend 
  * this class and define the getters for your endpoints.
+ * 
+ * If your Api does something a bit different, you can override the different 
+ * extractors or services, in order to fine-tune the process. Please read the 
+ * javadocs of the interfaces to get an idea of what to do.
  * 
  * @author Pablo Fernandez
  *
  */
 public abstract class DefaultApi10a implements Api
 {
-  
+  /**
+   * Returns the access token extractor.
+   * 
+   * @return access token extractor
+   */
   public AccessTokenExtractor getAccessTokenExtractor()
   {
     return new TokenExtractorImpl();
   }
 
+  /**
+   * Returns the base string extractor.
+   * 
+   * @return base string extractor
+   */
   public BaseStringExtractor getBaseStringExtractor()
   {
     return new BaseStringExtractorImpl();
   }
 
+  /**
+   * Returns the header extractor.
+   * 
+   * @return header extractor
+   */
   public HeaderExtractor getHeaderExtractor()
   {
     return new HeaderExtractorImpl();
   }
 
+  /**
+   * Returns the request token extractor.
+   * 
+   * @return request token extractor
+   */
   public RequestTokenExtractor getRequestTokenExtractor()
   {
     return new TokenExtractorImpl();
   }
 
+  /**
+   * Returns the signature service.
+   * 
+   * @return signature service
+   */
   public SignatureService getSignatureService()
   {
     return new HMACSha1SignatureService(); 
   }
 
+  /**
+   * Returns the timestamp service.
+   * 
+   * @return timestamp service
+   */
   public TimestampService getTimestampService()
   {
     return new TimestampServiceImpl();
   }
   
+  /**
+   * Returns the verb for the access token endpoint (defaults to POST)
+   * 
+   * @return access token endpoint verb
+   */
   public Verb getAccessTokenVerb()
   {
     return Verb.POST;
   }
   
+  /**
+   * Returns the verb for the request token endpoint (defaults to POST)
+   * 
+   * @return request token endpoint verb
+   */
   public Verb getRequestTokenVerb()
   {
     return Verb.POST;
