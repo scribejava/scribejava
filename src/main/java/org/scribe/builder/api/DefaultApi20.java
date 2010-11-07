a1146846fdc4f039eaaeb0fb24318f2990b8db24
Pablo Fernandez <fernandezpablo85@gmail.com>
2010-11-07 18:01:32 -0300

    Added plumbing code for 2.0

---
+++ src/main/java/org/scribe/builder/api/DefaultApi20.java
@@ -0,0 +1,14 @@
+package org.scribe.builder.api;
+
+import org.scribe.oauth.OAuthService;
+
+public class DefaultApi20 implements Api
+{
+
+  @Override
+  public OAuthService createService(String apiKey, String apiSecret, String callback, String scope)
+  {
+    return null;
+  }
+
+}
