22cb9f5dc648dcccf5fde9094d1ae1d8422997ca
Pablo Fernandez <fernandezpablo85@gmail.com>
2010-11-07 18:39:27 -0300

    Made 2.0 impl fit the internal refactor

--- src/main/java/org/scribe/builder/api/DefaultApi20.java
+++ src/main/java/org/scribe/builder/api/DefaultApi20.java
@@ -1,12 +1,13 @@
 package org.scribe.builder.api;
 
+import org.scribe.model.OAuthConfig;
 import org.scribe.oauth.OAuthService;
 
 public class DefaultApi20 implements Api
 {
 
   @Override
-  public OAuthService createService(String apiKey, String apiSecret, String callback, String scope)
+  public OAuthService createService(OAuthConfig config, String scope)
   {
     return null;
   }
