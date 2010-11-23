fc2c46bd3a361f03d042c961fb8b4ea8c8d68736
Pablo Fernandez <fernandezpablo85@gmail.com>
2010-11-23 17:35:42 -0300

    Changed GoogleApi to use GET

--- src/main/java/org/scribe/builder/api/GoogleApi.java
+++ src/main/java/org/scribe/builder/api/GoogleApi.java
@@ -1,5 +1,7 @@
 package org.scribe.builder.api;
 
+import org.scribe.model.*;
+
 public class GoogleApi extends DefaultApi10a
 {
   @Override
@@ -12,5 +14,17 @@ public class GoogleApi extends DefaultApi10a
   protected String getRequestTokenEndpoint()
   {
     return "https://www.google.com/accounts/OAuthGetRequestToken";
+  }
+
+  @Override
+  protected Verb getAccessTokenVerb()
+  {
+    return Verb.GET;
+  }
+
+  @Override
+  protected Verb getRequestTokenVerb()
+  {
+    return Verb.GET;
   }
 }
