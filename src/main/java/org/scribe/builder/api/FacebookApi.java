1f36fd0f32ca44e37d85cb226ee0da5a789f633b
Pablo Fernandez <fernandezpablo85@gmail.com>
2011-01-09 15:52:10 -0300

    Added Facebook Api and example

---
+++ src/main/java/org/scribe/builder/api/FacebookApi.java
@@ -0,0 +1,16 @@
+package org.scribe.builder.api;
+
+import org.scribe.exceptions.*;
+import org.scribe.model.*;
+
+public class FacebookApi extends DefaultApi20
+{
+  private static final String AUTHORIZE_URL = "https://graph.facebook.com/oauth/authorize?response_type=token&client_id=%s&redirect_uri=%s";
+  @Override
+  public String getAuthorizationUrl(OAuthConfig config)
+  {
+    if(OAuthConstants.OUT_OF_BAND.equals(config.getCallback()))
+    	throw new OAuthException("Facebook does not support oob authentication.");
+    return String.format(AUTHORIZE_URL, config.getApiKey(), config.getCallback());
+  }
+}
---
+++ src/test/java/org/scribe/examples/FacebookExample.java
@@ -0,0 +1,55 @@
+package org.scribe.examples;
+
+import java.util.*;
+
+import org.scribe.builder.*;
+import org.scribe.builder.api.*;
+import org.scribe.model.*;
+import org.scribe.oauth.*;
+
+public class FacebookExample
+{
+  private static final String NETWORK_NAME = "Facebook";
+  private static final String PROTECTED_RESOURCE_URL = "https://graph.facebook.com/me";
+  private static final String NO_SECRET_NEEDED = "";
+  private static final Token EMPTY_TOKEN = null;
+
+  public static void main(String[] args)
+  {
+    OAuthService service = new ServiceBuilder()
+                                  .provider(FacebookApi.class)
+                                  .apiKey("anonymous")
+                                  .apiSecret("anonymous")
+                                  .callback("http://www.example.com/oauth_callback/")
+                                  .build();
+    Scanner in = new Scanner(System.in);
+
+    System.out.println("=== " + NETWORK_NAME + "'s OAuth Workflow ===");
+    System.out.println();
+
+    // Obtain the Authorization URL
+    System.out.println("Fetching the Authorization URL...");
+    String authorizationUrl = service.getAuthorizationUrl(EMPTY_TOKEN);
+    System.out.println("Got the Authorization URL!");
+    System.out.println("Now go and authorize Scribe here:");
+    System.out.println(authorizationUrl);
+    System.out.println("And paste the access token here");
+    System.out.print(">>");
+    Token accessToken = new Token(in.nextLine(), NO_SECRET_NEEDED);
+    System.out.println();
+
+    // Now let's go and ask for a protected resource!
+    System.out.println("Now we're going to access a protected resource...");
+    OAuthRequest request = new OAuthRequest(Verb.GET, PROTECTED_RESOURCE_URL);
+    service.signRequest(accessToken, request);
+    Response response = request.send();
+    System.out.println("Got it! Lets see what we found...");
+    System.out.println();
+    System.out.println(response.getCode());
+    System.out.println(response.getBody());
+
+    System.out.println();
+    System.out.println("Thats it man! Go and build something awesome with Scribe! :)");
+
+  }
+}
