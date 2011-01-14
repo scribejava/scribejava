9c4f5bc33734da00a092d3694ce78cddf4dac9f6
Pablo Fernandez <fernandezpablo85@gmail.com>
2011-01-13 23:49:08 -0300

    Refactored OOB checking in fbook api

--- src/main/java/org/scribe/builder/api/FacebookApi.java
+++ src/main/java/org/scribe/builder/api/FacebookApi.java
@@ -1,17 +1,16 @@
 package org.scribe.builder.api;
 
-import org.scribe.exceptions.*;
 import org.scribe.model.*;
 import org.scribe.utils.*;
 
 public class FacebookApi extends DefaultApi20
 {
   private static final String AUTHORIZE_URL = "https://graph.facebook.com/oauth/authorize?response_type=token&client_id=%s&redirect_uri=%s";
+
   @Override
   public String getAuthorizationUrl(OAuthConfig config)
   {
-    if(OAuthConstants.OUT_OF_BAND.equals(config.getCallback()))
-    	throw new OAuthException("Facebook does not support oob authentication.");
+    Preconditions.checkValidUrl(config.getCallback(), "Must provide a valid url as callback. Facebook does not support OOB");
     return String.format(AUTHORIZE_URL, config.getApiKey(), URLUtils.percentEncode(config.getCallback()));
   }
 }
