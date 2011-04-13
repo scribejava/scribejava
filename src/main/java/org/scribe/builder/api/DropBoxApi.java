bfcf0655dab0b436d6be580e3e81c784d0984938
Pablo Fernandez <fernandezpablo85@gmail.com>
2011-04-13 19:03:00 -0300

    added Dropbox Api

---
+++ src/main/java/org/scribe/builder/api/DropBoxApi.java
@@ -0,0 +1,25 @@
+package org.scribe.builder.api;
+
+import org.scribe.model.*;
+
+public class DropBoxApi extends DefaultApi10a
+{
+  @Override
+  public String getAccessTokenEndpoint()
+  {
+    return "https://api.dropbox.com/0/oauth/access_token";
+  }
+
+  @Override
+  public String getAuthorizationUrl(Token requestToken)
+  {
+    return "https://www.dropbox.com/0/oauth/authorize?oauth_token="+requestToken.getToken();
+  }
+
+  @Override
+  public String getRequestTokenEndpoint()
+  {
+    return "https://api.dropbox.com/0/oauth/request_token";
+  }
+
+}
