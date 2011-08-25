27b1fc390b2cdec4d9c741ddab0762aa3d2b94d9
Chia-Wei Li <dreamcwli@gmail.com>
2011-08-25 13:43:10 +0800

    Fix MobilePlurkApi naming

--- src/main/java/org/scribe/builder/api/MobilePlurkApi.java
+++ src/main/java/org/scribe/builder/api/MobilePlurkApi.java
@@ -2,7 +2,7 @@ package org.scribe.builder.api;
 
 import org.scribe.model.Token;
 
-public class PlurkApi extends DefaultApi10a
+public class MobilePlurkApi extends DefaultApi10a
 {
   private static final String REQUEST_TOKEN_URL = "http://www.plurk.com/OAuth/request_token";
   private static final String AUTHORIZATION_URL = "http://www.plurk.com/m/authorize?oauth_token=%s";
