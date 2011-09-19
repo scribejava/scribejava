7c480f7f14f92b124657c55eee0a330ccad497ec
Casey W Crites <crites.casey@gmail.com>
2011-09-19 14:49:28 -0700

    Added support for the SimpleGeo api.

---
+++ src/main/java/org/scribe/builder/api/SimpleGeoApi.java
@@ -0,0 +1,22 @@
+package org.scribe.builder.api;
+
+import org.scribe.model.Token;
+
+public class SimpleGeoApi extends DefaultApi10a {
+
+	@Override
+	public String getRequestTokenEndpoint() {
+		return "";
+	}
+
+	@Override
+	public String getAccessTokenEndpoint() {
+		return "";
+	}
+
+	@Override
+	public String getAuthorizationUrl(Token requestToken) {
+		return "";
+	}
+
+}
