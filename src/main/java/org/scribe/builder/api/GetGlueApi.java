bef18c570e880e638d15003b5f78b49187e05f29
mason55 <mason@mason55.com>
2012-02-08 08:52:01 -0500

    Added GetGlue api

--- src/main/java/org/scribe/builder/api/GetGlueApi.java
+++ src/main/java/org/scribe/builder/api/GetGlueApi.java
@@ -2,11 +2,11 @@ package org.scribe.builder.api;
 
 import org.scribe.model.Token;
 
-public class MisoApi extends DefaultApi10a
+public class GetGlueApi extends DefaultApi10a
 {
-  private static final String AUTHORIZE_URL = "http://gomiso.com/oauth/authorize?oauth_token=%s";
-  private static final String REQUEST_TOKEN_RESOURCE = "http://gomiso.com/oauth/request_token";
-  private static final String ACCESS_TOKEN_RESOURCE = "http://gomiso.com/oauth/access_token";
+  private static final String AUTHORIZE_URL = "http://getglue.com/oauth/authorize?oauth_token=%s";
+  private static final String REQUEST_TOKEN_RESOURCE = "https://api.getglue.com/oauth/request_token";
+  private static final String ACCESS_TOKEN_RESOURCE = "https://api.getglue.com/oauth/access_token";
 
   @Override
   public String getAccessTokenEndpoint()
--- src/main/java/org/scribe/builder/api/TumblrApi.java
+++ src/main/java/org/scribe/builder/api/TumblrApi.java
@@ -7,7 +7,7 @@ public class TumblrApi extends DefaultApi10a
   private static final String AUTHORIZE_URL = "https://www.tumblr.com/oauth/authorize?oauth_token=%s";
   private static final String REQUEST_TOKEN_RESOURCE = "http://www.tumblr.com/oauth/request_token";
   private static final String ACCESS_TOKEN_RESOURCE = "http://www.tumblr.com/oauth/access_token";
-  
+
   @Override
   public String getAccessTokenEndpoint()
   {
