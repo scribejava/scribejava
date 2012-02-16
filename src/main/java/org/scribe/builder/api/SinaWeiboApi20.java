a5e330cd426af6b4754f3d6cbd859d68b43e4722
George Cao <matrix3456@163.com>
2012-02-16 16:24:46 +0800

    reformat the code

--- src/main/java/org/scribe/builder/api/SinaWeiboApi20.java
+++ src/main/java/org/scribe/builder/api/SinaWeiboApi20.java
@@ -1,38 +1,48 @@
 package org.scribe.builder.api;
 
-import org.scribe.extractors.AccessTokenExtractor;
-import org.scribe.extractors.JsonTokenExtractor;
-import org.scribe.model.OAuthConfig;
-import org.scribe.model.Verb;
+import org.scribe.extractors.*;
 
-import static org.scribe.utils.OAuthEncoder.encode;
+import org.scribe.model.*;
 
-public class SinaWeiboApi20 extends DefaultApi20 {
-    private static final String AUTHORIZE_URL = "https://api.weibo.com/oauth2/authorize?client_id=%s&redirect_uri=%s&response_type=code";
-    private static final String SCOPED_AUTHORIZE_URL = AUTHORIZE_URL + "&scope=%s";
+import org.scribe.utils.*;
 
-    @Override
-    public Verb getAccessTokenVerb() {
-        return Verb.POST;
+/**
+ * SinaWeibo OAuth 2.0 api.
+ */
+public class SinaWeiboApi20 extends DefaultApi20
+{
+  private static final String AUTHORIZE_URL = "https://api.weibo.com/oauth2/authorize?client_id=%s&redirect_uri=%s&response_type=code";
+  private static final String SCOPED_AUTHORIZE_URL = AUTHORIZE_URL + "&scope=%s";
+
+  @Override
+  public Verb getAccessTokenVerb()
+  {
+    return Verb.POST;
+  }
+
+  @Override
+  public AccessTokenExtractor getAccessTokenExtractor()
+  {
+    return new JsonTokenExtractor();
+  }
+
+  @Override
+  public String getAccessTokenEndpoint()
+  {
+    return "https://api.weibo.com/oauth2/access_token?grant_type=authorization_code";
+  }
+
+  @Override
+  public String getAuthorizationUrl(OAuthConfig config)
+  {
+    // Append scope if present
+    if(config.hasScope())
+    {
+      return String.format(SCOPED_AUTHORIZE_URL, config.getApiKey(), OAuthEncoder.encode(config.getCallback()), OAuthEncoder.encode(config.getScope()));
     }
-
-    @Override
-    public AccessTokenExtractor getAccessTokenExtractor() {
-        return new JsonTokenExtractor();
+    else
+    {
+      return String.format(AUTHORIZE_URL, config.getApiKey(), OAuthEncoder.encode(config.getCallback()));
     }
-
-    @Override
-    public String getAccessTokenEndpoint() {
-        return "https://api.weibo.com/oauth2/access_token?grant_type=authorization_code";
-    }
-
-    @Override
-    public String getAuthorizationUrl(OAuthConfig config) {
-        // Append scope if present
-        if (config.hasScope()) {
-            return String.format(SCOPED_AUTHORIZE_URL, config.getApiKey(), encode(config.getCallback()), encode(config.getScope()));
-        } else {
-            return String.format(AUTHORIZE_URL, config.getApiKey(), encode(config.getCallback()));
-        }
-    }
+  }
 }
