a004a66ec8497007ca72fd922bb135fb88432af0
kongo09 <kongo09@gmail.com>
2012-03-10 17:57:19 +0100

    fixed XING api

--- src/main/java/org/scribe/builder/api/XingApi.java
+++ src/main/java/org/scribe/builder/api/XingApi.java
@@ -1,6 +1,5 @@
-package de.feedfabrik.book.apis.xing.oauth;
+package org.scribe.builder.api;
 
-import org.scribe.builder.api.DefaultApi10a;
 import org.scribe.model.Token;
 
 /**
@@ -9,25 +8,28 @@ import org.scribe.model.Token;
  * @author <a href="https://www.xing.com/profile/Patrick_Alberts">Patrick Alberts</a>
  * @see <a href="https://dev.xing.com/">XING API</a>
  */
-public class XingApi extends DefaultApi10a {
-
+public class XingApi extends DefaultApi10a
+{
 	private static final String API_ENDPOINT = "https://api.xing.com/v1";
 	private static final String REQUEST_TOKEN_RESOURCE = "/request_token";
 	private static final String AUTHORIZE_URL = "/authorize?oauth_token=%s";
 	private static final String ACCESS_TOKEN_RESOURCE = "/access_token";
 
 	@Override
-	public String getAccessTokenEndpoint() {
+	public String getAccessTokenEndpoint()
+	{
 		return API_ENDPOINT + ACCESS_TOKEN_RESOURCE;
 	}
 
 	@Override
-	public String getRequestTokenEndpoint() {
+	public String getRequestTokenEndpoint()
+	{
 		return API_ENDPOINT + REQUEST_TOKEN_RESOURCE;
 	}
 
 	@Override
-	public String getAuthorizationUrl(Token requestToken) {
+	public String getAuthorizationUrl(Token requestToken)
+	{
 		return String.format(API_ENDPOINT + AUTHORIZE_URL, requestToken.getToken());
 	}
 }
