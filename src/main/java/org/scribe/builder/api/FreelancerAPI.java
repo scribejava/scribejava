3132a9dfe7f9db00edbf9b19f132b64a6b25cc9c
ponyloky <ponyloky@gmail.com>
2012-04-05 00:44:54 -0300

    Adding support for the Freelancer.com API

---
+++ src/main/java/org/scribe/builder/api/FreelancerAPI.java
@@ -0,0 +1,39 @@
+package org.scribe.builder.api;
+
+import org.scribe.model.Token;
+import org.scribe.model.Verb;
+
+public class FreelancerAPI extends DefaultApi10a
+{
+  private static final String AUTHORIZATION_URL = "http://www.sandbox.freelancer.com/users/api-token/auth.php?oauth_token=%s";
+
+  @Override
+  public String getAccessTokenEndpoint()
+  {
+    return "http://api.sandbox.freelancer.com/RequestAccessToken/requestAccessToken.xml?";
+  }
+
+  @Override
+  public String getRequestTokenEndpoint()
+  {
+    return "http://api.sandbox.freelancer.com/RequestRequestToken/requestRequestToken.xml";
+  }
+
+  @Override
+  public Verb getAccessTokenVerb()
+  {
+    return Verb.GET;
+  }
+
+  @Override
+  public Verb getRequestTokenVerb()
+  {
+    return Verb.GET;
+  }
+
+  @Override
+  public String getAuthorizationUrl(Token requestToken)
+  {
+    return String.format(AUTHORIZATION_URL, requestToken.getToken());
+  }
+}
