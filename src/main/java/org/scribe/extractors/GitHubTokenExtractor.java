a74bec794553f0e42f3fcb4eeeff5dfa5723e217
yosaku01 <hunterster@gmail.com>
2012-05-17 14:24:05 +0800

    add the example of calling github api with scrible-java.

---
+++ src/main/java/org/scribe/extractors/GitHubTokenExtractor.java
@@ -0,0 +1,27 @@
+package org.scribe.extractors;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.scribe.exceptions.OAuthException;
+import org.scribe.model.Token;
+import org.scribe.utils.Preconditions;
+
+public class GitHubTokenExtractor implements AccessTokenExtractor
+{
+	
+	private Pattern accessTokenPattern = Pattern.compile("access_token=([^&]+)");
+	public Token extract(String response)
+	{
+		Preconditions.checkEmptyString(response, "Cannot extract a token from a null or empty String");
+		Matcher matcher = accessTokenPattern.matcher(response);
+		if(matcher.find())
+		{
+		      return new Token(matcher.group(1), "", response);
+		}
+		else
+		{
+		      throw new OAuthException("Cannot extract an acces token. Response was: " + response);
+		}
+	}	
+}
