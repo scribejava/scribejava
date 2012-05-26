745938e18d3583f9a8c0f9d280990c198b4cc0c6
yosaku01 <hunterster@gmail.com>
2012-05-26 13:29:27 +0800

    updated the source code's format with eclipse's format function.

--- src/main/java/org/scribe/builder/api/GitHubApi.java
+++ src/main/java/org/scribe/builder/api/GitHubApi.java
@@ -4,42 +4,37 @@ import org.scribe.extractors.*;
 import org.scribe.model.*;
 import org.scribe.utils.*;
 
-public class GitHubApi extends DefaultApi20 
-{
+public class GitHubApi extends DefaultApi20 {
 
-	private static final String AUTHORIZE_URL = "https://github.com/login/oauth/authorize?client_id=%s";	
-	private static final String SCOPED_AUTHORIZE_URL = AUTHORIZE_URL + "&scope=%s";	
-	
+	private static final String AUTHORIZE_URL = "https://github.com/login/oauth/authorize?client_id=%s";
+	private static final String SCOPED_AUTHORIZE_URL = AUTHORIZE_URL
+			+ "&scope=%s";
+
 	@Override
-	public String getAccessTokenEndpoint() 
-	{
+	public String getAccessTokenEndpoint() {
 		return "https://github.com/login/oauth/access_token";
 	}
 
 	@Override
-	public String getAuthorizationUrl(OAuthConfig config) 
-	{
+	public String getAuthorizationUrl(OAuthConfig config) {
 		// Append scope if present
-	    if (config.hasScope())
-	    {
-	    	return String.format(SCOPED_AUTHORIZE_URL, config.getApiKey(), OAuthEncoder.encode(config.getCallback()), 
-	    			OAuthEncoder.encode(config.getScope()));
-	    }
-	    else
-	    {
-	    	return String.format(AUTHORIZE_URL, config.getApiKey(), OAuthEncoder.encode(config.getCallback()));
-	    }
+		if (config.hasScope()) {
+			return String.format(SCOPED_AUTHORIZE_URL, config.getApiKey(),
+					OAuthEncoder.encode(config.getCallback()),
+					OAuthEncoder.encode(config.getScope()));
+		} else {
+			return String.format(AUTHORIZE_URL, config.getApiKey(),
+					OAuthEncoder.encode(config.getCallback()));
+		}
 	}
-	
-	 @Override
-	 public Verb getAccessTokenVerb()
-	 {
-		 return Verb.POST;
-	 }
-	 
-	 @Override
-	 public GitHubTokenExtractor getAccessTokenExtractor()
-	 {
-		 return new GitHubTokenExtractor();
-	 }
+
+	@Override
+	public Verb getAccessTokenVerb() {
+		return Verb.POST;
+	}
+
+	@Override
+	public GitHubTokenExtractor getAccessTokenExtractor() {
+		return new GitHubTokenExtractor();
+	}
 }
