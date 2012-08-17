a121c3aa47a5d158f33aebba110e1d81a24ca8f3
eranation <ehrann.mehdan@gmail.com>
2012-08-17 00:18:07 -0300

    Update src/main/java/org/scribe/builder/api/ConstantContactApi2.java
    It didn't work for getting the token... 2 reasons, 
    1: missing grant_type in the requeest
    2: token extractor wasn't expecting a JSON response 

--- src/main/java/org/scribe/builder/api/ConstantContactApi2.java
+++ src/main/java/org/scribe/builder/api/ConstantContactApi2.java
@@ -1,7 +1,15 @@
 package org.scribe.builder.api;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.scribe.exceptions.OAuthException;
+import org.scribe.extractors.AccessTokenExtractor;
 import org.scribe.model.OAuthConfig;
+import org.scribe.model.Token;
+import org.scribe.model.Verb;
 import org.scribe.utils.OAuthEncoder;
+import org.scribe.utils.Preconditions;
 
 public class ConstantContactApi2 extends DefaultApi20 
 {
@@ -11,12 +19,41 @@ public class ConstantContactApi2 extends DefaultApi20
 	@Override
 	public String getAccessTokenEndpoint() 
 	{
-		return "https://oauth2.constantcontact.com/oauth2/oauth/token";
+		return "https://oauth2.constantcontact.com/oauth2/oauth/token?grant_type=authorization_code";
 	}
 
+	
 	@Override
 	public String getAuthorizationUrl(OAuthConfig config) 
 	{
 		return String.format(AUTHORIZE_URL, config.getApiKey(), OAuthEncoder.encode(config.getCallback()));
 	}
+	
+    @Override
+    public Verb getAccessTokenVerb() {
+        return Verb.POST;
+    }
+    
+    @Override
+    public AccessTokenExtractor getAccessTokenExtractor() {
+        return new AccessTokenExtractor() {
+            
+            @Override
+            public Token extract(String response) {
+                Preconditions.checkEmptyString(response, "Response body is incorrect. Can't extract a token from an empty string");
+
+                String regex = "\"access_token\"\\s*:\\s*\"([^&\"]+)\"";
+				Matcher matcher = Pattern.compile(regex).matcher(response);
+                if (matcher.find())
+                {
+                  String token = OAuthEncoder.decode(matcher.group(1));
+                  return new Token(token, "", response);
+                } 
+                else
+                {
+                  throw new OAuthException("Response body is incorrect. Can't extract a token from this: '" + response + "'", null);
+                }
+            }
+        };
+    }
 }
