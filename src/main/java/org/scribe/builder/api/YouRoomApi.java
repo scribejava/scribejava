98da206883e0c1c3dcc8253b4c08f57ae61274af
Shintaro Katafuchi <hot.chemistry21@gmail.com>
2012-09-02 21:29:12 +0900

    add YouRoomApi.java.

---
+++ src/main/java/org/scribe/builder/api/YouRoomApi.java
@@ -0,0 +1,25 @@
+package org.scribe.builder.api;
+
+import org.scribe.model.Token;
+import org.scribe.utils.OAuthEncoder;
+
+public class YouRoomApi extends DefaultApi10a {
+	private static final String ACCESS_TOKEN_RESOURCE = "https://www.youroom.in/oauth/access_token";
+	private static final String REQUEST_TOKEN_RESOURCE = "https://www.youroom.in/oauth/request_token";
+	private static final String AUTHORIZE_URL = "https://www.youroom.in/oauth/authorize/?oauth_token=%s";
+	  
+	@Override
+	public String getAccessTokenEndpoint() {
+		return ACCESS_TOKEN_RESOURCE; 
+	}
+	
+	@Override
+	public String getRequestTokenEndpoint() {
+		return REQUEST_TOKEN_RESOURCE;
+	}
+
+	@Override
+	public String getAuthorizationUrl(Token requestToken) {
+		return String.format(AUTHORIZE_URL, OAuthEncoder.encode(requestToken.getToken()));
+	}
+}
