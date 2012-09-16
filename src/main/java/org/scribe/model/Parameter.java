fa31853a9abec95bd145ea441687b973b6e95675
Julio Gutierrez <bubuntux@gmail.com>
2012-09-16 01:26:17 -0500

    forking the project

--- src/main/java/org/scribe/model/Parameter.java
+++ src/main/java/org/scribe/model/Parameter.java
@@ -1,8 +1,5 @@
 package org.scribe.model;
 
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-import org.scribe.exceptions.OAuthException;
 import org.scribe.utils.OAuthEncoder;
 
 /**
@@ -10,9 +7,9 @@ import org.scribe.utils.OAuthEncoder;
  */
 public class Parameter implements Comparable<Parameter>
 {
-  private static final String UTF = "UTF8";
 
   private final String key;
+
   private final String value;
 
   public Parameter(String key, String value)
@@ -23,16 +20,20 @@ public class Parameter implements Comparable<Parameter>
 
   public String asUrlEncodedPair()
   {
-    return OAuthEncoder.encode(key).concat("=").concat(OAuthEncoder.encode(value));
+    return OAuthEncoder.encode(key).concat("=")
+        .concat(OAuthEncoder.encode(value));
   }
-  
+
   public boolean equals(Object other)
   {
-    if(other == null) return false;
-    if(other == this) return true;
-    if(!(other instanceof Parameter)) return false;
-    
-    Parameter otherParam = (Parameter) other;
+    if (other == null)
+      return false;
+    if (other == this)
+      return true;
+    if (!(other instanceof Parameter))
+      return false;
+
+    Parameter otherParam = (Parameter)other;
     return otherParam.key.equals(key) && otherParam.value.equals(value);
   }
 
