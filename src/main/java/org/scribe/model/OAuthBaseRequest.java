d3ba3ce2db4bc23c623000e9a4561f82361e44e2
Nathan Esquenazi <nesquena@gmail.com>
2013-06-06 23:01:14 -0700

    Adds javadocs to the interface OAuthBaseRequest

--- src/main/java/org/scribe/model/OAuthBaseRequest.java
+++ src/main/java/org/scribe/model/OAuthBaseRequest.java
@@ -3,16 +3,73 @@ package org.scribe.model;
 import java.util.Map;
 
 public interface OAuthBaseRequest {
-
+    /**
+     * Adds an OAuth parameter.
+     *
+     * @param key name of the parameter
+     * @param value value of the parameter
+     *
+     * @throws IllegalArgumentException if the parameter is not an OAuth parameter
+     */
     public void addOAuthParameter(String key, String value);
+
+    /**
+     * Returns the {@link Map} containing the key-value pair of parameters.
+     *
+     * @return parameters as map
+     */
     public Map<String, String> getOauthParameters();
 
+    /**
+     * Returns the complete url (host + resource + encoded querystring parameters).
+     *
+     * @return the complete url.
+     */
     public String getCompleteUrl();
+
+    /**
+     * Add an HTTP Header to the Request
+     *
+     * @param key the header name
+     * @param value the header value
+     */
     public void addHeader(String key, String value);
+
+    /**
+     * Add a QueryString parameter
+     *
+     * @param key the parameter name
+     * @param value the parameter value
+     */
     public void addQuerystringParameter(String key, String value);
+
+    /**
+     * Returns the URL without the port and the query string part.
+     *
+     * @return the OAuth-sanitized URL
+     */
     public String getSanitizedUrl();
+
+    /**
+     * Get a {@link ParameterList} with the query string parameters.
+     *
+     * @return a {@link ParameterList} containing the query string parameters.
+     * @throws OAuthException if the request URL is not valid.
+     */
     public ParameterList getQueryStringParams();
+
+    /**
+     * Returns the HTTP Verb
+     *
+     * @return the verb
+     */
     public Verb getVerb();
+
+    /**
+     * Obtains a {@link ParameterList} of the body parameters.
+     *
+     * @return a {@link ParameterList}containing the body parameters.
+     */
     public ParameterList getBodyParams();
 
     public String toString();
