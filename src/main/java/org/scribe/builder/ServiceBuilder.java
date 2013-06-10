49da5bfe54acca1fa0d102559d065a0989bc86c0
Sergey Tchernatkin <s.chernatkin@hh.ru>
2013-06-10 17:03:51 +0400

    HH-34569 formatting

--- src/main/java/org/scribe/builder/ServiceBuilder.java
+++ src/main/java/org/scribe/builder/ServiceBuilder.java
@@ -8,14 +8,11 @@ import org.scribe.oauth.*;
 import org.scribe.utils.*;
 
 /**
- * Implementation of the Builder pattern, with a fluent interface that creates a
- * {@link OAuthService}
- * 
- * @author Pablo Fernandez
+ * Implementation of the Builder pattern, with a fluent interface that creates a {@link OAuthService}
  *
+ * @author  Pablo Fernandez
  */
-public class ServiceBuilder
-{
+public class ServiceBuilder {
   private String apiKey;
   private String apiSecret;
   private String callback;
@@ -23,106 +20,97 @@ public class ServiceBuilder
   private String scope;
   private SignatureType signatureType;
   private OutputStream debugStream;
-  
-  /**
-   * Default constructor
-   */
-  public ServiceBuilder()
-  {
+
+  /** Default constructor */
+  public ServiceBuilder() {
     this.callback = OAuthConstants.OUT_OF_BAND;
     this.signatureType = SignatureType.Header;
     this.debugStream = null;
   }
-  
+
   /**
    * Configures the {@link Api}
-   * 
-   * @param apiClass the class of one of the existent {@link Api}s on org.scribe.api package
-   * @return the {@link ServiceBuilder} instance for method chaining
+   *
+   * @param  apiClass  the class of one of the existent {@link Api}s on org.scribe.api package
+   *
+   * @return  the {@link ServiceBuilder} instance for method chaining
    */
-  public ServiceBuilder provider(Class<? extends Api> apiClass)
-  {
+  public ServiceBuilder provider(Class<? extends Api> apiClass) {
     this.api = createApi(apiClass);
     return this;
   }
 
-  private Api createApi(Class<? extends Api> apiClass)
-  {
+  private Api createApi(Class<? extends Api> apiClass) {
     Preconditions.checkNotNull(apiClass, "Api class cannot be null");
     Api api;
-    try
-    {
-      api = apiClass.newInstance();  
-    }
-    catch(Exception e)
-    {
+    try {
+      api = apiClass.newInstance();
+    } catch (Exception e) {
       throw new OAuthException("Error while creating the Api object", e);
     }
     return api;
   }
 
   /**
-   * Configures the {@link Api}
+   * Configures the {@link Api} Overloaded version. Let's you use an instance instead of a class.
    *
-   * Overloaded version. Let's you use an instance instead of a class.
+   * @param  api  instance of {@link Api}s
    *
-   * @param api instance of {@link Api}s
-   * @return the {@link ServiceBuilder} instance for method chaining
+   * @return  the {@link ServiceBuilder} instance for method chaining
    */
-  public ServiceBuilder provider(Api api)
-  {
-	  Preconditions.checkNotNull(api, "Api cannot be null");
-	  this.api = api;
-	  return this;
+  public ServiceBuilder provider(Api api) {
+    Preconditions.checkNotNull(api, "Api cannot be null");
+    this.api = api;
+    return this;
   }
 
   /**
    * Adds an OAuth callback url
-   * 
-   * @param callback callback url. Must be a valid url or 'oob' for out of band OAuth
-   * @return the {@link ServiceBuilder} instance for method chaining
+   *
+   * @param  callback  callback url. Must be a valid url or 'oob' for out of band OAuth
+   *
+   * @return  the {@link ServiceBuilder} instance for method chaining
    */
-  public ServiceBuilder callback(String callback)
-  {
+  public ServiceBuilder callback(String callback) {
     Preconditions.checkNotNull(callback, "Callback can't be null");
     this.callback = callback;
     return this;
   }
-  
+
   /**
    * Configures the api key
-   * 
-   * @param apiKey The api key for your application
-   * @return the {@link ServiceBuilder} instance for method chaining
+   *
+   * @param  apiKey  The api key for your application
+   *
+   * @return  the {@link ServiceBuilder} instance for method chaining
    */
-  public ServiceBuilder apiKey(String apiKey)
-  {
+  public ServiceBuilder apiKey(String apiKey) {
     Preconditions.checkEmptyString(apiKey, "Invalid Api key");
     this.apiKey = apiKey;
     return this;
   }
-  
+
   /**
    * Configures the api secret
-   * 
-   * @param apiSecret The api secret for your application
-   * @return the {@link ServiceBuilder} instance for method chaining
+   *
+   * @param  apiSecret  The api secret for your application
+   *
+   * @return  the {@link ServiceBuilder} instance for method chaining
    */
-  public ServiceBuilder apiSecret(String apiSecret)
-  {
+  public ServiceBuilder apiSecret(String apiSecret) {
     Preconditions.checkEmptyString(apiSecret, "Invalid Api secret");
     this.apiSecret = apiSecret;
     return this;
   }
-  
+
   /**
    * Configures the OAuth scope. This is only necessary in some APIs (like Google's).
-   * 
-   * @param scope The OAuth scope
-   * @return the {@link ServiceBuilder} instance for method chaining
+   *
+   * @param  scope  The OAuth scope
+   *
+   * @return  the {@link ServiceBuilder} instance for method chaining
    */
-  public ServiceBuilder scope(String scope)
-  {
+  public ServiceBuilder scope(String scope) {
     Preconditions.checkEmptyString(scope, "Invalid OAuth scope");
     this.scope = scope;
     return this;
@@ -131,36 +119,33 @@ public class ServiceBuilder
   /**
    * Configures the signature type, choose between header, querystring, etc. Defaults to Header
    *
-   * @param scope The OAuth scope
-   * @return the {@link ServiceBuilder} instance for method chaining
+   * @param  scope  The OAuth scope
+   *
+   * @return  the {@link ServiceBuilder} instance for method chaining
    */
-  public ServiceBuilder signatureType(SignatureType type)
-  {
+  public ServiceBuilder signatureType(SignatureType type) {
     Preconditions.checkNotNull(type, "Signature type can't be null");
     this.signatureType = type;
     return this;
   }
 
-  public ServiceBuilder debugStream(OutputStream stream)
-  {
+  public ServiceBuilder debugStream(OutputStream stream) {
     Preconditions.checkNotNull(stream, "debug stream can't be null");
     this.debugStream = stream;
     return this;
   }
 
-  public ServiceBuilder debug()
-  {
+  public ServiceBuilder debug() {
     this.debugStream(System.out);
     return this;
   }
-  
+
   /**
    * Returns the fully configured {@link OAuthService}
-   * 
-   * @return fully configured {@link OAuthService}
+   *
+   * @return  fully configured {@link OAuthService}
    */
-  public OAuthService build()
-  {
+  public OAuthService build() {
     Preconditions.checkNotNull(api, "You must specify a valid api through the provider() method");
     Preconditions.checkEmptyString(apiKey, "You must provide an api key");
     Preconditions.checkEmptyString(apiSecret, "You must provide an api secret");
--- src/main/java/org/scribe/model/OAuthConfig.java
+++ src/main/java/org/scribe/model/OAuthConfig.java
@@ -4,25 +4,22 @@ import java.io.*;
 
 /**
  * Parameter object that groups OAuth config values
- * 
- * @author Pablo Fernandez
+ *
+ * @author  Pablo Fernandez
  */
-public class OAuthConfig
-{
+public class OAuthConfig {
   private final String apiKey;
   private final String apiSecret;
   private final String callback;
   private final SignatureType signatureType;
   private final String scope;
   private final OutputStream debugStream;
-  
-  public OAuthConfig(String key, String secret)
-  {
+
+  public OAuthConfig(String key, String secret) {
     this(key, secret, null, null, null, null);
   }
 
-  public OAuthConfig(String key, String secret, String callback, SignatureType type, String scope, OutputStream stream)
-  {
+  public OAuthConfig(String key, String secret, String callback, SignatureType type, String scope, OutputStream stream) {
     this.apiKey = key;
     this.apiSecret = secret;
     this.callback = callback;
@@ -31,47 +28,36 @@ public class OAuthConfig
     this.debugStream = stream;
   }
 
-  public String getApiKey()
-  {
+  public String getApiKey() {
     return apiKey;
   }
 
-  public String getApiSecret()
-  {
+  public String getApiSecret() {
     return apiSecret;
   }
 
-  public String getCallback()
-  {
+  public String getCallback() {
     return callback;
   }
 
-  public SignatureType getSignatureType()
-  {
+  public SignatureType getSignatureType() {
     return signatureType;
   }
 
-  public String getScope()
-  {
+  public String getScope() {
     return scope;
   }
 
-  public boolean hasScope()
-  {
+  public boolean hasScope() {
     return scope != null;
   }
 
-  public void log(String message)
-  {
-    if (debugStream != null)
-    {
+  public void log(String message) {
+    if (debugStream != null) {
       message = message + "\n";
-      try
-      {
+      try {
         debugStream.write(message.getBytes("UTF8"));
-      }
-      catch (Exception e)
-      {
+      } catch (Exception e) {
         throw new RuntimeException("there were problems while writting to the debug stream", e);
       }
     }
