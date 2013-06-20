24ae317b98a222c53752d0e84e3ab821f661f6e2
Houman <houman001@gmail.com>
2013-06-20 12:55:41 +1000

    Testing refresh token...

--- src/test/java/org/scribe/examples/Google2Example.java
+++ src/test/java/org/scribe/examples/Google2Example.java
@@ -20,6 +20,10 @@ public class Google2Example
 
     public static void main(String[] args)
     {
+        Token accessToken = null;
+        Verifier verifier = null;
+        boolean refresh = true;
+
         // Put your own API key here.
         // String apiKey = "407408718192.apps.googleusercontent.com"; // Demo
         String apiKey = "800271916124.apps.googleusercontent.com";
@@ -37,35 +41,51 @@ public class Google2Example
         System.out.println("=== " + NETWORK_NAME + "'s OAuth Workflow ===");
         System.out.println();
 
-        // Obtain the Authorization URL
-        System.out.println("Fetching the Authorization URL...");
-        String authorizationUrl = service.getAuthorizationUrl(EMPTY_TOKEN);
-        System.out.println("Got the Authorization URL!");
-        System.out.println("Now go and authorize Scribe here:");
-        System.out.println(authorizationUrl);
-        System.out.println("And paste the authorization code here");
-        System.out.print(">>");
-        Verifier verifier = new Verifier(in.nextLine());
-        System.out.println();
+        if (refresh)
+        {
+            accessToken = new Token("ya29.AHES6ZSxvPdXzZIHEVvA2OjeNwlYLM2FyC16U2R10MkKuIpvuAmZQg",
+                    "1/UstOOW3JTfI_TbfRscwVVdAhguTNoSHSRcFXNOy4ZEQ");
+        }
+        else
+        {
+            // Obtain the Authorization URL
+            System.out.println("Fetching the Authorization URL...");
+            String authorizationUrl = service.getAuthorizationUrl(EMPTY_TOKEN);
+            System.out.println("Got the Authorization URL!");
+            System.out.println("Now go and authorize Scribe here:");
+            System.out.println(authorizationUrl);
+            System.out.println("And paste the authorization code here");
+            System.out.print(">>");
+            verifier = new Verifier(in.nextLine());
+            System.out.println();
 
-        // Trade the Request Token and Verfier for the Access Token
-        System.out.println("Trading the Request Token for an Access Token...");
-        Token accessToken = service.getAccessToken(EMPTY_TOKEN, verifier);
-        System.out.println("Got the Access Token!");
-        System.out.println("(if your curious it looks like this: " + accessToken + " )");
-        System.out.println();
+            // Trade the Request Token and Verfier for the Access Token
+            System.out.println("Trading the Request Token for an Access Token...");
+            accessToken = service.getAccessToken(EMPTY_TOKEN, verifier);
+            System.out.println("Got the Access Token!");
+            System.out.println("(if your curious it looks like this: " + accessToken + " )");
+            System.out.println();
+        }
 
-        // Trade the Refresh Token for a new Access Token
-        System.out.println("Trading the Refresh Token for a new Access Token...");
-        Token newAccessToken = service.getAccessToken(accessToken, verifier);
-        System.out.println("Got the Access Token!");
-        System.out.println("(if your curious it looks like this: " + newAccessToken + " )");
-        System.out.println();
+        try
+        {
+            // Trade the Refresh Token for a new Access Token
+            System.out.println("Trading the Refresh Token for a new Access Token...");
+            Token newAccessToken = service.getAccessToken(accessToken, verifier);
+            System.out.println("Got the Access Token!");
+            System.out.println("(if your curious it looks like this: " + newAccessToken + " )");
+            System.out.println();
+            accessToken = newAccessToken;
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
 
         // Now let's go and ask for a protected resource!
         System.out.println("Now we're going to access a protected resource...");
         OAuthRequest request = new OAuthRequest(Verb.GET, PROTECTED_RESOURCE_URL);
-        service.signRequest(newAccessToken, request);
+        service.signRequest(accessToken, request);
         Response response = request.send();
         System.out.println("Got it! Lets see what we found...");
         System.out.println();
