7970028dda259345d9ce25ce7c10db2c2c688e3d
Ludovic Claude <ludovic.claude54@googlemail.com>
2013-07-21 00:41:26 +0200

    Add expireWhen to Token

--- src/main/java/org/scribe/extractors/JsonTokenExtractor.java
+++ src/main/java/org/scribe/extractors/JsonTokenExtractor.java
@@ -1,5 +1,6 @@
 package org.scribe.extractors;
 
+import java.util.Date;
 import java.util.regex.*;
 
 import org.scribe.exceptions.*;
@@ -9,14 +10,23 @@ import org.scribe.utils.*;
 public class JsonTokenExtractor implements AccessTokenExtractor
 {
   private Pattern accessTokenPattern = Pattern.compile("\"access_token\":\\s*\"(\\S*?)\"");
+  private static final Pattern expirePattern = Pattern.compile("\"expires_in\":\\s*(\\d+)");
 
   public Token extract(String response)
   {
     Preconditions.checkEmptyString(response, "Cannot extract a token from a null or empty String");
+
     Matcher matcher = accessTokenPattern.matcher(response);
     if(matcher.find())
     {
-      return new Token(matcher.group(1), "", response);
+      Date expireDate = null;
+      Matcher expireMatcher = expirePattern.matcher(response);
+      if (expireMatcher.find())
+      {
+        long expiration = Long.parseLong(expireMatcher.group(1));
+        expireDate = new Date(System.currentTimeMillis() + expiration * 1000);
+      }
+      return new Token(matcher.group(1), "", response, expireDate);
     }
     else
     {
