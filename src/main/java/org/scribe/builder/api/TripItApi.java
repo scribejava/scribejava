d92ca1dcf5bf124ba522cec239317520ef0a73a4
kmahoney <corp@styxdigital.com>
2013-09-03 07:00:18 -0700

    Added TripIt API

---
+++ src/main/java/org/scribe/builder/api/TripItApi.java
@@ -0,0 +1,10 @@
+package org.scribe.builder.api;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: madkrupt
+ * Date: 9/2/13
+ * Time: 3:05 PM
+ */
+public class TripItApi {
+}
---
+++ src/test/java/org/scribe/examples/TripItExample.java
@@ -0,0 +1,10 @@
+package org.scribe.examples;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: madkrupt
+ * Date: 9/2/13
+ * Time: 3:14 PM
+ */
+public class TripItExample {
+}
