d38572cf05766a9ab825e5bd19ebedafd853d31b
Stas Gromov <s.gromov@hh.ru>
2014-03-11 19:10:50 +0400

    improve Google 2.0 Example a bit [d.chekmarev]

--- src/test/java/org/scribe/examples/Google20Example.java
+++ src/test/java/org/scribe/examples/Google20Example.java
@@ -10,21 +10,21 @@ import org.scribe.model.Verb;
 import org.scribe.model.Verifier;
 import org.scribe.oauth.OAuth20ServiceImpl;
 
-public class Google20Example {
+public abstract class Google20Example {
 
     private static final String NETWORK_NAME = "G+";
     private static final String PROTECTED_RESOURCE_URL = "https://www.googleapis.com/plus/v1/people/me";
     private static final String STATE_PARAM_NAME = "state";
     private static final Token EMPTY_TOKEN = null;
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         // Replace these with your own api key and secret
         final String clientId = "api key";
         final String apiSecret = "api secret";
         final OAuth20ServiceImpl service = (OAuth20ServiceImpl) new ServiceBuilder().provider(GoogleApi20.class).apiKey(
                 clientId).apiSecret(apiSecret).scope("profile") // replace with desired scope
                 .grantType("authorization_code").callback("http://example.com/callback").build();
-        Scanner in = new Scanner(System.in);
+        final Scanner in = new Scanner(System.in, "UTF-8");
 
         System.out.println("=== " + NETWORK_NAME + "'s OAuth Workflow ===");
         System.out.println();
@@ -38,12 +38,12 @@ public class Google20Example {
         System.out.println(authorizationUrl);
         System.out.println("And paste the authorization code here");
         System.out.print(">>");
-        Verifier verifier = new Verifier(in.nextLine());
+        final Verifier verifier = new Verifier(in.nextLine());
         System.out.println();
 
         // Trade the Request Token and Verfier for the Access Token
         System.out.println("Trading the Request Token for an Access Token...");
-        Token accessToken = service.getAccessToken(EMPTY_TOKEN, verifier);
+        final Token accessToken = service.getAccessToken(EMPTY_TOKEN, verifier);
         System.out.println("Got the Access Token!");
         System.out.println("(if your curious it looks like this: " + accessToken + " )");
         System.out.println();
@@ -51,12 +51,21 @@ public class Google20Example {
         // Now let's go and ask for a protected resource!
         System.out.println("Now we're going to access a protected resource...");
         while (true) {
-            System.out.println("Paste profile query for fetch");
+            System.out.println("Paste fieldnames to fetch (leave empty to get profile, 'exit' to stop example)");
             System.out.print(">>");
             final String query = in.nextLine();
             System.out.println();
 
-            final OAuthRequest request = new OAuthRequest(Verb.GET, String.format(PROTECTED_RESOURCE_URL, query));
+            final String requestUrl;
+            if ("exit".equals(query)) {
+                break;
+            } else if (query == null || query.isEmpty()) {
+                requestUrl = PROTECTED_RESOURCE_URL;
+            } else {
+                requestUrl = PROTECTED_RESOURCE_URL + "?fields=" + query;
+            }
+
+            final OAuthRequest request = new OAuthRequest(Verb.GET, requestUrl);
             service.signRequest(accessToken, request);
             final Response response = request.send();
             System.out.println();
