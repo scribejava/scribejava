268114890faa7905d709968540bd84f9bc1c8e8c
Narendra Umate <narendraumate@gmail.com>
2014-04-18 20:03:33 -0400

    Added POST support to getAccessToken.
    Added GRANT_TYPE and AUTHORIZATION_CODE constants.

--- src/main/java/org/scribe/model/OAuthConstants.java
+++ src/main/java/org/scribe/model/OAuthConstants.java
@@ -48,5 +48,7 @@ public class OAuthConstants
   public static final String CLIENT_SECRET = "client_secret";
   public static final String REDIRECT_URI = "redirect_uri";
   public static final String CODE = "code";
-  
+  public static final String GRANT_TYPE = "grant_type";
+  public static final String AUTHORIZATION_CODE = "authorization_code";
+
 }
