b55b08b8f7c3b6cc205ccb64710bea7ef84315ed
Stas Gromov <s.gromov@hh.ru>
2014-06-20 20:29:24 +0400

    format

--- src/main/java/org/scribe/builder/api/LinkedInApi20.java
+++ src/main/java/org/scribe/builder/api/LinkedInApi20.java
@@ -10,39 +10,40 @@ import org.scribe.utils.OAuthEncoder;
 import org.scribe.utils.Preconditions;
 
 public class LinkedInApi20 extends DefaultApi20 {
-  private static final String AUTHORIZE_URL = "https://www.linkedin.com/uas/oauth2/authorization?response_type=code&client_id=%s&redirect_uri=%s";
 
-  private static final String SCOPED_AUTHORIZE_URL = AUTHORIZE_URL + "&scope=%s";
+    private static final String AUTHORIZE_URL = "https://www.linkedin.com/uas/oauth2/authorization?response_type=code&client_id=%s&redirect_uri=%s";
 
-  @Override
-  public Verb getAccessTokenVerb() {
-    return Verb.GET;
-  }
+    private static final String SCOPED_AUTHORIZE_URL = AUTHORIZE_URL + "&scope=%s";
 
-  @Override
-  public String getAccessTokenEndpoint() {
-    return "https://www.linkedin.com/uas/oauth2/accessToken";
-  }
-
-  @Override
-  public String getAuthorizationUrl(final OAuthConfig config) {
-    Preconditions.checkValidUrl(config.getCallback(), "Must provide a valid url as callback. LinkedIn does not support OOB");
-
-    if (config.hasScope()) {
-      return String.format(
-        SCOPED_AUTHORIZE_URL, config.getApiKey(), OAuthEncoder.encode(config.getCallback()), OAuthEncoder.encode(config.getScope()));
-    } else {
-      return String.format(AUTHORIZE_URL, config.getApiKey(), OAuthEncoder.encode(config.getCallback()));
+    @Override
+    public Verb getAccessTokenVerb() {
+        return Verb.GET;
     }
-  }
 
-  @Override
-  public AccessTokenExtractor getAccessTokenExtractor() {
-    return new JsonTokenExtractor();
-  }
+    @Override
+    public String getAccessTokenEndpoint() {
+        return "https://www.linkedin.com/uas/oauth2/accessToken";
+    }
 
-  @Override
-  public OAuthService createService(final OAuthConfig config) {
-    return new LinkedIn20ServiceImpl(this, config);
-  }
+    @Override
+    public String getAuthorizationUrl(final OAuthConfig config) {
+        Preconditions.checkValidUrl(config.getCallback(), "Must provide a valid url as callback. LinkedIn does not support OOB");
+
+        if (config.hasScope()) {
+            return String.format(
+                    SCOPED_AUTHORIZE_URL, config.getApiKey(), OAuthEncoder.encode(config.getCallback()), OAuthEncoder.encode(config.getScope()));
+        } else {
+            return String.format(AUTHORIZE_URL, config.getApiKey(), OAuthEncoder.encode(config.getCallback()));
+        }
+    }
+
+    @Override
+    public AccessTokenExtractor getAccessTokenExtractor() {
+        return new JsonTokenExtractor();
+    }
+
+    @Override
+    public OAuthService createService(final OAuthConfig config) {
+        return new LinkedIn20ServiceImpl(this, config);
+    }
 }
