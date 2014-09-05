7d518359ba603f400a16ae0e805ab171316c7305
Stas Gromov <s.gromov@hh.ru>
2014-09-05 14:13:13 +0400

    HH-46254 format

--- src/main/java/org/scribe/builder/api/VkontakteApi.java
+++ src/main/java/org/scribe/builder/api/VkontakteApi.java
@@ -7,31 +7,32 @@ import org.scribe.utils.OAuthEncoder;
 import org.scribe.utils.Preconditions;
 
 /**
- * @author  Boris G. Tsirkin <mail@dotbg.name>
- * @since  20.4.2011
+ * @author Boris G. Tsirkin <mail@dotbg.name>
+ * @since 20.4.2011
  */
 public class VkontakteApi extends DefaultApi20 {
-  private static final String AUTHORIZE_URL = "https://oauth.vk.com/authorize?client_id=%s&redirect_uri=%s&response_type=code";
-  private static final String SCOPED_AUTHORIZE_URL = String.format("%s&scope=%%s", AUTHORIZE_URL);
 
-  @Override
-  public String getAccessTokenEndpoint() {
-    return "https://oauth.vk.com/access_token";
-  }
+    private static final String AUTHORIZE_URL = "https://oauth.vk.com/authorize?client_id=%s&redirect_uri=%s&response_type=code";
+    private static final String SCOPED_AUTHORIZE_URL = String.format("%s&scope=%%s", AUTHORIZE_URL);
 
-  @Override
-  public String getAuthorizationUrl(OAuthConfig config) {
-    Preconditions.checkValidUrl(config.getCallback(), "Valid url is required for a callback. Vkontakte does not support OOB");
-    if (config.hasScope()) { // Appending scope if present
-      return String.format(
-        SCOPED_AUTHORIZE_URL, config.getApiKey(), OAuthEncoder.encode(config.getCallback()), OAuthEncoder.encode(config.getScope()));
-    } else {
-      return String.format(AUTHORIZE_URL, config.getApiKey(), OAuthEncoder.encode(config.getCallback()));
+    @Override
+    public String getAccessTokenEndpoint() {
+        return "https://oauth.vk.com/access_token";
     }
-  }
 
-  @Override
-  public AccessTokenExtractor getAccessTokenExtractor() {
-    return new JsonTokenExtractor();
-  }
+    @Override
+    public String getAuthorizationUrl(OAuthConfig config) {
+        Preconditions.checkValidUrl(config.getCallback(), "Valid url is required for a callback. Vkontakte does not support OOB");
+        if (config.hasScope()) { // Appending scope if present
+            return String.format(
+                    SCOPED_AUTHORIZE_URL, config.getApiKey(), OAuthEncoder.encode(config.getCallback()), OAuthEncoder.encode(config.getScope()));
+        } else {
+            return String.format(AUTHORIZE_URL, config.getApiKey(), OAuthEncoder.encode(config.getCallback()));
+        }
+    }
+
+    @Override
+    public AccessTokenExtractor getAccessTokenExtractor() {
+        return new JsonTokenExtractor();
+    }
 }
