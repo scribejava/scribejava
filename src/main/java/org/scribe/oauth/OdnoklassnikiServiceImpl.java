f14b2f3e098eb3f89edb01e95c75f273640bdda0
Stas Gromov <s.gromov@hh.ru>
2014-09-19 14:42:31 +0400

    format

--- src/main/java/org/scribe/oauth/OdnoklassnikiServiceImpl.java
+++ src/main/java/org/scribe/oauth/OdnoklassnikiServiceImpl.java
@@ -10,26 +10,27 @@ import org.scribe.model.OAuthRequest;
 import org.scribe.model.Token;
 
 public class OdnoklassnikiServiceImpl extends OAuth20ServiceImpl {
-  public OdnoklassnikiServiceImpl(final DefaultApi20 api, final OAuthConfig config) {
-    super(api, config);
-  }
 
-  @Override
-  public void signRequest(final Token accessToken, final OAuthRequest request) {
-    // sig = md5( request_params_composed_string+ md5(access_token + application_secret_key)  )
-    try {
-      final String tokenDigest = md5Hex((accessToken.getToken() + getConfig().getApiSecret()));
-
-      final String completeUrl = request.getCompleteUrl();
-      final int queryIndex = completeUrl.indexOf('?');
-      if (queryIndex != -1) {
-        final String sigSource = URLDecoder.decode(completeUrl.substring(queryIndex + 1).replace("&", ""), CharEncoding.UTF_8) + tokenDigest;
-        request.addQuerystringParameter("sig", md5Hex(sigSource));
-      }
-
-      super.signRequest(accessToken, request);
-    } catch (UnsupportedEncodingException unex) {
-      throw new IllegalStateException(unex);
+    public OdnoklassnikiServiceImpl(final DefaultApi20 api, final OAuthConfig config) {
+        super(api, config);
     }
-  }
+
+    @Override
+    public void signRequest(final Token accessToken, final OAuthRequest request) {
+        // sig = md5( request_params_composed_string+ md5(access_token + application_secret_key)  )
+        try {
+            final String tokenDigest = md5Hex((accessToken.getToken() + getConfig().getApiSecret()));
+
+            final String completeUrl = request.getCompleteUrl();
+            final int queryIndex = completeUrl.indexOf('?');
+            if (queryIndex != -1) {
+                final String sigSource = URLDecoder.decode(completeUrl.substring(queryIndex + 1).replace("&", ""), CharEncoding.UTF_8) + tokenDigest;
+                request.addQuerystringParameter("sig", md5Hex(sigSource));
+            }
+
+            super.signRequest(accessToken, request);
+        } catch (UnsupportedEncodingException unex) {
+            throw new IllegalStateException(unex);
+        }
+    }
 }
