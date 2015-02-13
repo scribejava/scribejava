ab3256918db48c8bb66f2a186ba2d74cc644f874
Chaitra Rao <chaitra_rao@intuit.com>
2015-02-13 14:05:08 +0530

    Added oauth example for GitHub

---
+++ src/test/java/org/scribe/examples/GitHubExample.java
@@ -0,0 +1,64 @@
+package org.scribe.examples;
+
+import java.util.Scanner;
+
+import org.scribe.builder.ServiceBuilder;
+import org.scribe.builder.api.GitHubApi;
+import org.scribe.model.OAuthRequest;
+import org.scribe.model.Response;
+import org.scribe.model.Token;
+import org.scribe.model.Verb;
+import org.scribe.model.Verifier;
+import org.scribe.oauth.OAuthService;
+
+public class GitHubExample {
+	private static final String PROTECTED_RESOURCE_URL = "https://api.github.com/users";
+	private static final Token EMPTY_TOKEN = null;
+
+	public static void main(String[] args) {
+		OAuthService service = new ServiceBuilder().provider(GitHubApi.class)
+				.apiKey("43cd422b1d8931d29c0d")
+				.apiSecret("14809f746c14a423542169d1b2f4f7983928290b")
+				.callback("https://scribe_oauth_github").build();
+
+		Scanner in = new Scanner(System.in);
+
+		System.out.println("=== Github's OAuth Workflow ===");
+		System.out.println();
+
+		// Obtain the Authorization URL
+		System.out.println("Fetching the Authorization URL...");
+		String authorizationUrl = service.getAuthorizationUrl(EMPTY_TOKEN);
+		System.out.println("Got the Authorization URL!");
+
+		System.out.println("Now go and authorize Scribe here:");
+		System.out.println(authorizationUrl);
+		System.out.println("And paste the authorization code here");
+		System.out.print(">>");
+		Verifier verifier = new Verifier(in.nextLine());
+		System.out.println();
+
+		// Trade the Request Token and Verfier for the Access Token
+		System.out.println("Trading the Request Token for an Access Token...");
+		Token accessToken = service.getAccessToken(EMPTY_TOKEN, verifier);
+		System.out.println("Got the Access Token!");
+		System.out.println("(if your curious it looks like this: "
+				+ accessToken + " )");
+		System.out.println();
+
+		// Now let's go and ask for a protected resource!
+		System.out.println("Now we're going to access a protected resource...");
+		OAuthRequest request = new OAuthRequest(Verb.GET,
+				PROTECTED_RESOURCE_URL);
+		service.signRequest(accessToken, request);
+		Response response = request.send();
+		System.out.println("Got it! Lets see what we found...");
+		System.out.println();
+		System.out.println(response.getCode());
+		System.out.println(response.getBody());
+
+		System.out.println();
+		System.out
+				.println("Thats it man! Go and build something awesome with Scribe! :)");
+	}
+}
