b88b9ddf805954be6abf3b0e4c0641a9f6edd803
Pablo Fernandez <fernandezpablo85@gmail.com>
2015-07-13 12:17:21 -0300

    remove apache license header, it's wrong, license is MIT

--- src/main/java/org/scribe/model/OAuthConstants.java
+++ src/main/java/org/scribe/model/OAuthConstants.java
@@ -1,23 +1,8 @@
-/* 
-Copyright 2010 Pablo Fernandez
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
- */
 package org.scribe.model;
 
 /**
  * This class contains OAuth constants, used project-wide
- * 
+ *
  * @author Pablo Fernandez
  */
 public class OAuthConstants
@@ -48,5 +33,5 @@ public class OAuthConstants
   public static final String CLIENT_SECRET = "client_secret";
   public static final String REDIRECT_URI = "redirect_uri";
   public static final String CODE = "code";
-  
+
 }
