8c1c98da48347941613402b131e6ff67cf98c51d
arribeth <thibaut.arribe@kelis.fr>
2015-10-28 23:23:35 +0100

    Update extraction to get token type

--- src/main/java/org/scribe/extractors/JsonTokenExtractor.java
+++ src/main/java/org/scribe/extractors/JsonTokenExtractor.java
@@ -8,15 +8,18 @@ import org.scribe.utils.*;
 
 public class JsonTokenExtractor implements AccessTokenExtractor
 {
-  private Pattern accessTokenPattern = Pattern.compile("\"access_token\":\\s*\"(\\S*?)\"");
+  private Pattern accessTokenPattern = Pattern.compile("\"access_token\"\\s*:\\s*\"(\\S*?)\"");
+  private Pattern tokenTypePattern = Pattern.compile("\"token_type\"\\s*:\\s*\"(\\S*?)\"");
 
   public Token extract(String response)
   {
     Preconditions.checkEmptyString(response, "Cannot extract a token from a null or empty String");
-    Matcher matcher = accessTokenPattern.matcher(response);
-    if(matcher.find())
+    Matcher matcherAccessToken = accessTokenPattern.matcher(response);
+    Matcher matcherTokenType = tokenTypePattern.matcher(response);
+    if(matcherAccessToken.find())
     {
-      return new Token(matcher.group(1), "", response);
+      if(matcherTokenType.find()) return new Token(matcherAccessToken.group(1), matcherTokenType.group(1), response);
+      else return new Token(matcherAccessToken.group(1), "", response);
     }
     else
     {
