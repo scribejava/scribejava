4203c56d81b44c978f396e768479ffb951eb80f2
arribeth <thibaut.arribe@kelis.fr>
2015-10-29 00:10:07 +0100

    debug + support Bearer oauth 2 request signing (+ queryString to support Facebook)

--- src/main/java/org/scribe/oauth/OAuth20ServiceImpl.java
+++ src/main/java/org/scribe/oauth/OAuth20ServiceImpl.java
@@ -9,10 +9,11 @@ import org.scribe.services.Base64Encoder;
 public class OAuth20ServiceImpl implements OAuthService
 {
   private static final String VERSION = "2.0";
-  
+
   private final DefaultApi20 api;
+
   private final OAuthConfig config;
-  
+
   /**
    * Default constructor
    * 
@@ -30,56 +31,60 @@ public class OAuth20ServiceImpl implements OAuthService
    */
   public Token getAccessToken(Token requestToken, Verifier verifier)
   {
-	Verb verb = api.getAccessTokenVerb();
-	OAuthRequest request = new OAuthRequest(verb, api.getAccessTokenEndpoint());
-	Response response;
-	switch (api.getClientAuthenticationType()) {
-	case Header :
-		StringBuilder stringBuilder = new StringBuilder();
-		stringBuilder.append("Basic ");
-		stringBuilder.append(Base64Encoder.getInstance().encode((config.getApiKey() + ":" + config.getApiSecret()).getBytes(Charset.forName("UTF-8"))));
-		request.addHeader("Authorization", stringBuilder.toString());
-		break;
-	
-	case PostForm :
-		if(verb!=Verb.POST && verb!=Verb.PUT) throw new IllegalArgumentException("Impossible to set parameter in a form with an other HTTP method than POST or PUT");
-		request.addBodyParameter(OAuthConstants.CLIENT_ID, config.getApiKey());
-		request.addBodyParameter(OAuthConstants.CLIENT_SECRET, config.getApiSecret());
-		break;
-		
-	case QueryString :
-		request.addQuerystringParameter(OAuthConstants.CLIENT_ID, config.getApiKey());
-		request.addQuerystringParameter(OAuthConstants.CLIENT_SECRET, config.getApiSecret());
-		break;
-	}
-	
-	switch (api.getParameterType()) {
-	case Header :
-		request.addHeader(OAuthConstants.CLIENT_ID, config.getApiKey());
-		request.addHeader(OAuthConstants.CLIENT_SECRET, config.getApiSecret());
-		request.addHeader(OAuthConstants.CODE, verifier.getValue());
-		request.addHeader(OAuthConstants.REDIRECT_URI, config.getCallback());
-		if (config.hasScope()) request.addHeader(OAuthConstants.SCOPE, config.getScope());
-		break;
-	
-	case PostForm :
-		if(verb!=Verb.POST && verb!=Verb.PUT) throw new IllegalArgumentException("Impossible to set parameter in a form with an other HTTP method than POST or PUT");
-		request.addBodyParameter(OAuthConstants.GRANT_TYPE, OAuthConstants.AUTHORIZATION_CODE);
-		request.addBodyParameter(OAuthConstants.CODE, verifier.getValue());
-		request.addBodyParameter(OAuthConstants.REDIRECT_URI, config.getCallback());
-		request.addBodyParameter(OAuthConstants.CLIENT_ID, config.getApiKey());
-		if (config.hasScope()) request.addBodyParameter(OAuthConstants.SCOPE, config.getScope());
-		break;
-		
-	case QueryString :
-		request.addQuerystringParameter(OAuthConstants.CLIENT_ID, config.getApiKey());
-		request.addQuerystringParameter(OAuthConstants.CLIENT_SECRET, config.getApiSecret());
-		request.addQuerystringParameter(OAuthConstants.CODE, verifier.getValue());
-		request.addQuerystringParameter(OAuthConstants.REDIRECT_URI, config.getCallback());
-		if (config.hasScope()) request.addQuerystringParameter(OAuthConstants.SCOPE, config.getScope());
-		break;
-	}
-	response = api.handleRequest(request).send();
+    Verb verb = api.getAccessTokenVerb();
+    OAuthRequest request = new OAuthRequest(verb, api.getAccessTokenEndpoint());
+    Response response;
+    switch (api.getClientAuthenticationType())
+    {
+    case Header:
+      StringBuilder stringBuilder = new StringBuilder();
+      stringBuilder.append("Basic ");
+      stringBuilder.append(Base64Encoder.getInstance().encode((config.getApiKey() + ":" + config.getApiSecret()).getBytes(Charset.forName("UTF-8"))));
+      request.addHeader("Authorization", stringBuilder.toString());
+      break;
+
+    case PostForm:
+      if (verb != Verb.POST && verb != Verb.PUT)
+        throw new IllegalArgumentException("Impossible to set parameter in a form with an other HTTP method than POST or PUT");
+      request.addBodyParameter(OAuthConstants.CLIENT_ID, config.getApiKey());
+      request.addBodyParameter(OAuthConstants.CLIENT_SECRET, config.getApiSecret());
+      break;
+
+    case QueryString:
+      request.addQuerystringParameter(OAuthConstants.CLIENT_ID, config.getApiKey());
+      request.addQuerystringParameter(OAuthConstants.CLIENT_SECRET, config.getApiSecret());
+      break;
+    }
+
+    switch (api.getParameterType())
+    {
+    case Header:
+      request.addHeader(OAuthConstants.GRANT_TYPE, OAuthConstants.AUTHORIZATION_CODE);
+      request.addHeader(OAuthConstants.CODE, verifier.getValue());
+      request.addHeader(OAuthConstants.REDIRECT_URI, config.getCallback());
+      if (config.hasScope())
+        request.addHeader(OAuthConstants.SCOPE, config.getScope());
+      break;
+
+    case PostForm:
+      if (verb != Verb.POST && verb != Verb.PUT)
+        throw new IllegalArgumentException("Impossible to set parameter in a form with an other HTTP method than POST or PUT");
+      request.addBodyParameter(OAuthConstants.GRANT_TYPE, OAuthConstants.AUTHORIZATION_CODE);
+      request.addBodyParameter(OAuthConstants.CODE, verifier.getValue());
+      request.addBodyParameter(OAuthConstants.REDIRECT_URI, config.getCallback());
+      if (config.hasScope())
+        request.addBodyParameter(OAuthConstants.SCOPE, config.getScope());
+      break;
+
+    case QueryString:
+      request.addQuerystringParameter(OAuthConstants.GRANT_TYPE, OAuthConstants.AUTHORIZATION_CODE);
+      request.addQuerystringParameter(OAuthConstants.CODE, verifier.getValue());
+      request.addQuerystringParameter(OAuthConstants.REDIRECT_URI, config.getCallback());
+      if (config.hasScope())
+        request.addQuerystringParameter(OAuthConstants.SCOPE, config.getScope());
+      break;
+    }
+    response = api.handleRequest(request).send();
     return api.getAccessTokenExtractor().extract(response.getBody());
   }
 
@@ -104,7 +109,22 @@ public class OAuth20ServiceImpl implements OAuthService
    */
   public void signRequest(Token accessToken, OAuthRequest request)
   {
-    request.addQuerystringParameter(OAuthConstants.ACCESS_TOKEN, accessToken.getToken());
+    // Not Oauth2 but usefull for some pseudo-oauth2 providers
+    if (config.getSignatureType().equals(SignatureType.QueryString)|| api.getSignatureType() == SignatureType.QueryString)
+      request.addQuerystringParameter(OAuthConstants.ACCESS_TOKEN, accessToken.getToken());
+    else
+    {
+      // With Oauth 2, the empty secret field is used to store the token type
+      if (accessToken.getSecret().equals(OAuthConstants.BEARER))
+      {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Bearer ");
+        stringBuilder.append(accessToken.getToken());
+        request.addHeader("Authorization", stringBuilder.toString());
+      }
+      else
+        throw new UnsupportedOperationException("Unsupported OAuth2 access token type");
+    }
   }
 
   /**
