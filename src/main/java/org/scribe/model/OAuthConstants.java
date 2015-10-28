9219de94558d9c03b4b59d9349c32b155e2500e2
arribeth <thibaut.arribe@kelis.fr>
2015-10-28 23:23:56 +0100

    Add Bearer Token type to constant

--- src/main/java/org/scribe/model/OAuthConstants.java
+++ src/main/java/org/scribe/model/OAuthConstants.java
@@ -35,5 +35,7 @@ public class OAuthConstants
   public static final String CODE = "code";
   public static final String GRANT_TYPE = "grant_type";
   public static final String AUTHORIZATION_CODE = "authorization_code";
+  public static final String BEARER = "Bearer";
+
 
 }
