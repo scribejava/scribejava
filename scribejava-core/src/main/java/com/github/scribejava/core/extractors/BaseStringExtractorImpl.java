cd4888e2a24e4281ceaa1da0ebdfe02fc9b4128b
Matyas Novy <mnovy@users.noreply.github.com>
2015-12-17 22:24:41 +0100

    Updated to be able to merge into master.

--- scribejava-core/src/main/java/com/github/scribejava/core/extractors/BaseStringExtractorImpl.java
+++ scribejava-core/src/main/java/com/github/scribejava/core/extractors/BaseStringExtractorImpl.java
@@ -14,20 +14,28 @@ import com.github.scribejava.core.utils.Preconditions;
  */
 public class BaseStringExtractorImpl implements BaseStringExtractor {
 
-    private static final String AMPERSAND_SEPARATED_STRING = "%s&%s&%s";
+  protected static final String AMPERSAND_SEPARATED_STRING = "%s&%s&%s";
 
     /**
      * {@inheritDoc}
      */
     public String extract(AbstractRequest request) {
         checkPreconditions(request);
-        String verb = OAuthEncoder.encode(request.getVerb().name());
-        String url = OAuthEncoder.encode(request.getSanitizedUrl());
+        String verb = OAuthEncoder.encode(getVerb(request));
+        String url = OAuthEncoder.encode(getUrl(request));
         String params = getSortedAndEncodedParams(request);
         return String.format(AMPERSAND_SEPARATED_STRING, verb, url, params);
     }
 
-    private String getSortedAndEncodedParams(AbstractRequest request) {
+    protected String getVerb(AbstractRequest request) {
+        return request.getVerb().name();
+    }
+
+    protected String getUrl(AbstractRequest request) {
+        return request.getSanitizedUrl();
+    }
+
+    protected String getSortedAndEncodedParams(AbstractRequest request) {
         ParameterList params = new ParameterList();
         params.addAll(request.getQueryStringParams());
         params.addAll(request.getBodyParams());
@@ -35,7 +43,7 @@ public class BaseStringExtractorImpl implements BaseStringExtractor {
         return params.sort().asOauthBaseString();
     }
 
-    private void checkPreconditions(AbstractRequest request) {
+    protected void checkPreconditions(AbstractRequest request) {
         Preconditions.checkNotNull(request, "Cannot extract base string from a null object");
 
         if (request.getOauthParameters() == null || request.getOauthParameters().size() <= 0) {
