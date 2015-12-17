aa5c340efe334e551c81a99f8815e039a908a0fe
Stas Gromov <s.gromov@hh.ru>
2015-12-17 11:41:42 +0300

    make getStream protected in Response

--- scribejava-core/src/main/java/com/github/scribejava/core/model/Response.java
+++ scribejava-core/src/main/java/com/github/scribejava/core/model/Response.java
@@ -68,7 +68,7 @@ public class Response {
      *
      * @return input stream / error stream
      */
-    private InputStream getStream() {
+    protected InputStream getStream() {
         return stream;
     }
 
