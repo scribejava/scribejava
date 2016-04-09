338406dc2c716e15f96aae43614628f8b285502f
Tomasz Szymaniec <tomek.szymaniec@gmail.com>
2016-04-09 22:30:01 +0100

    add Genius.com API authentication (OAuth2)

---
+++ scribejava-apis/src/main/java/com/github/scribejava/apis/service/GeniusOAuthServiceImpl.java
@@ -0,0 +1,19 @@
+package com.github.scribejava.apis.service;
+
+import com.github.scribejava.core.builder.api.DefaultApi20;
+import com.github.scribejava.core.model.AbstractRequest;
+import com.github.scribejava.core.model.OAuth2AccessToken;
+import com.github.scribejava.core.model.OAuthConfig;
+import com.github.scribejava.core.oauth.OAuth20Service;
+
+public class GeniusOAuthServiceImpl extends OAuth20Service {
+
+    public GeniusOAuthServiceImpl(DefaultApi20 api, OAuthConfig config) {
+        super(api, config);
+    }
+
+    @Override
+    public void signRequest(OAuth2AccessToken accessToken, AbstractRequest request) {
+        request.addHeader("Authorization", "Bearer " + accessToken.getAccessToken());
+    }
+}
