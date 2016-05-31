d014b52252a3c6d44ea64200f72d508a935f6867
Arto Toppi <arto.toppi@rocrok.com>
2016-05-31 13:41:50 +0800

    MisfitApi - Implement getAuthorizationBaseUrl
    Updates based on the code review:
    Removed unnecessary override for getAuthorizationUrl
    Implement method getAuthorizationBaseUrl

--- scribejava-apis/src/main/java/com/github/scribejava/apis/MisfitApi.java
+++ scribejava-apis/src/main/java/com/github/scribejava/apis/MisfitApi.java
@@ -26,31 +26,7 @@ public class MisfitApi extends DefaultApi20 {
     }
 
     @Override
-    public String getAuthorizationUrl(OAuthConfig config, Map<String, String> additionalParams) {
-        final ParameterList parameters = new ParameterList(additionalParams);
-        parameters.add(OAuthConstants.RESPONSE_TYPE, "code");
-        parameters.add(OAuthConstants.CLIENT_ID, config.getApiKey());
-
-        final String callback = config.getCallback();
-        if (callback != null) {
-            parameters.add(OAuthConstants.REDIRECT_URI, callback);
-        }
-
-        final String scope = config.getScope();
-        if (scope != null) {
-            parameters.add(OAuthConstants.SCOPE, scope);
-        }
-
-        final String state = config.getState();
-        if (state != null) {
-            parameters.add(OAuthConstants.STATE, state);
-        }
-
-        return parameters.appendTo("https://api.misfitwearables.com/auth/dialog/authorize");
-    }
-
-    @Override
     protected String getAuthorizationBaseUrl() {
-        throw new UnsupportedOperationException("use getAuthorizationUrl instead");
+        return "https://api.misfitwearables.com/auth/dialog/authorize";
     }
 }
