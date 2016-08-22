33cf3f5f15215429883bdcac596f9f47e2091c8d
alex <akorolkov@moysklad.ru>
2016-08-22 22:52:33 +0300

    fix OdnoklassnikiServiceImpl signature, params for hash must be sorted in lexicographic order, see http://new.apiok.ru/dev/methods/

--- scribejava-apis/src/main/java/com/github/scribejava/apis/service/OdnoklassnikiServiceImpl.java
+++ scribejava-apis/src/main/java/com/github/scribejava/apis/service/OdnoklassnikiServiceImpl.java
@@ -9,6 +9,7 @@ import com.github.scribejava.core.model.AbstractRequest;
 import com.github.scribejava.core.model.OAuth2AccessToken;
 import com.github.scribejava.core.model.OAuthConfig;
 import com.github.scribejava.core.oauth.OAuth20Service;
+import java.util.Arrays;
 
 public class OdnoklassnikiServiceImpl extends OAuth20Service {
 
@@ -18,17 +19,22 @@ public class OdnoklassnikiServiceImpl extends OAuth20Service {
 
     @Override
     public void signRequest(OAuth2AccessToken accessToken, AbstractRequest request) {
-        // sig = md5( request_params_composed_string+ md5(access_token + application_secret_key)  )
+        //sig = lower(md5( sorted_request_params_composed_string + md5(access_token + application_secret_key)))
         try {
             final String tokenDigest = md5Hex(accessToken.getAccessToken() + getConfig().getApiSecret());
 
             final String completeUrl = request.getCompleteUrl();
             final int queryIndex = completeUrl.indexOf('?');
             if (queryIndex != -1) {
-                final String sigSource
-                        = URLDecoder.decode(completeUrl.substring(queryIndex + 1).replace("&", ""), CharEncoding.UTF_8)
-                        + tokenDigest;
-                request.addQuerystringParameter("sig", md5Hex(sigSource));
+                final String[] params = completeUrl.substring(queryIndex + 1).split("&");
+                Arrays.sort(params);
+                final StringBuilder builder = new StringBuilder();
+                for (String param : params) {
+                    builder.append(param);
+                }
+
+                final String sigSource = URLDecoder.decode(builder.toString(), CharEncoding.UTF_8) + tokenDigest;
+                request.addQuerystringParameter("sig", md5Hex(sigSource).toLowerCase());
             }
 
             super.signRequest(accessToken, request);
