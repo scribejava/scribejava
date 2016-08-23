5a10327fe6e6b543d8f98b8a412dff5fcf037d40
asKorolkov <a.s.korolkov@tinkoff.ru>
2016-08-23 15:22:53 +0300

    review comments

--- scribejava-apis/src/test/java/com/github/scribejava/apis/serviceTest/OdnoklassnikiServiceTest.java
+++ scribejava-apis/src/test/java/com/github/scribejava/apis/serviceTest/OdnoklassnikiServiceTest.java
@@ -1,21 +1,24 @@
-package com.github.scribejava.apis.serviceTest;
+package com.github.scribejava.apis.servicetest;
 
 import com.github.scribejava.apis.OdnoklassnikiApi;
 import com.github.scribejava.core.builder.ServiceBuilder;
-import com.github.scribejava.core.model.*;
+
+import com.github.scribejava.core.model.OAuth2AccessToken;
+import com.github.scribejava.core.model.OAuthRequest;
+import com.github.scribejava.core.model.ParameterList;
+import com.github.scribejava.core.model.Verb;
+import com.github.scribejava.core.model.Parameter;
 import com.github.scribejava.core.oauth.OAuth20Service;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-/**
- * Created by alex on 22.08.16.
- */
 public class OdnoklassnikiServiceTest {
 
-    private OAuth20Service service;
     private static final String URL
-            = "https://api.ok.ru/fb.do?method=friends.get&fields=uid%2Cfirst_name%2Clast_name%2Cpic_2&application_key=AAAAAAAAAAAAAAAA&format=json";
+            = "https://api.ok.ru/fb.do?method=friends.get&fields=uid%2C" +
+            "first_name%2Clast_name%2Cpic_2&application_key=AAAAAAAAAAAAAAAA&format=json";
+    private OAuth20Service service;
 
     @Before
     public void setUp() {
@@ -29,14 +32,9 @@ public class OdnoklassnikiServiceTest {
 
     @Test
     public void shouldThrowExceptionIfBaseStringIsNull() {
-        OAuth2AccessToken accessToken = new OAuth2AccessToken("d3iwa.403gvrs194740652m1k4w2a503k3c");
-        OAuthRequest request = new OAuthRequest(Verb.GET, URL, service);
+        final OAuth2AccessToken accessToken = new OAuth2AccessToken("d3iwa.403gvrs194740652m1k4w2a503k3c");
+        final OAuthRequest request = new OAuthRequest(Verb.GET, URL, service);
         service.signRequest(accessToken, request);
-
-        //session_secret_key = md5Hex(accessToken.getAccessToken() + service.getConfig().getApiSecret()) == 129ea5bb368dfb321dc90f63da01b742
-        //значения_параметров  == application_key=AAAAAAAAAAAAAAAAfields=uid%2Cfirst_name%2Clast_name%2Cpic_2format=jsonmethod=friends.get
-        //sig == MD5(URLDecoder.decode(значения_параметров, CharEncoding.UTF_8) + session_secret_key) == 96127f5ca29a8351399e94bbd284ab16
-
         Assert.assertEquals(findParam(request.getQueryStringParams(), "sig"), "96127f5ca29a8351399e94bbd284ab16");
     }
 
@@ -49,5 +47,4 @@ public class OdnoklassnikiServiceTest {
         }
         return value;
     }
-
 }
