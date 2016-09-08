39aaa571234294d03cd8bd74500f6327c2052ce8
choo_co <choo_co@naver.com>
2016-09-08 12:16:37 +0900

    Add Naver API

---
+++ scribejava-apis/src/main/java/com/github/scribejava/apis/NaverApi.java
@@ -0,0 +1,27 @@
+package com.github.scribejava.apis;
+
+import com.github.scribejava.core.builder.api.DefaultApi20;
+
+public class NaverApi extends DefaultApi20 {
+	protected NaverApi() {
+	}
+
+	private static class InstanceHolder {
+		private static final NaverApi INSTANCE = new NaverApi();
+
+		private InstanceHolder() {
+		}
+	}
+
+	public static NaverApi instance() {
+		return NaverApi.InstanceHolder.INSTANCE;
+	}
+
+	public String getAccessTokenEndpoint() {
+		return "https://nid.naver.com/oauth2.0/token?grant_type=authorization_code";
+	}
+
+	protected String getAuthorizationBaseUrl() {
+		return "https://nid.naver.com/oauth2.0/authorize";
+	}
+}
