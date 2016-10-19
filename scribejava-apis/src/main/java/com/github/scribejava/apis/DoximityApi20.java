e6b5e3e4f2c20f2e5e8420861742b6304647c878
Bastien <bastien@medici.md>
2016-10-19 18:03:51 +0200

    Do some changes after pull request, simplify code.

--- scribejava-apis/src/main/java/com/github/scribejava/apis/DoximityApi20.java
+++ scribejava-apis/src/main/java/com/github/scribejava/apis/DoximityApi20.java
@@ -3,11 +3,7 @@ package com.github.scribejava.apis;
 import com.github.scribejava.apis.service.Doximity20ServiceImpl;
 import com.github.scribejava.core.builder.api.DefaultApi20;
 import com.github.scribejava.core.model.OAuthConfig;
-import com.github.scribejava.core.model.OAuthConstants;
-import com.github.scribejava.core.model.ParameterList;
 import com.github.scribejava.core.oauth.OAuth20Service;
-
-import java.util.Map;
 
 public class DoximityApi20 extends DefaultApi20 {
 
@@ -24,37 +20,12 @@ public class DoximityApi20 extends DefaultApi20 {
 
     @Override
     public String getAccessTokenEndpoint() {
-        return "https://auth.doximity.com/oauth/token?grant_type=authorization_code";
+        return "https://auth.doximity.com/oauth/token";
     }
 
     @Override
     protected String getAuthorizationBaseUrl() {
-        return "https://auth.doximity.com/oauth/authorize?";
-    }
-
-    @Override
-    public String getAuthorizationUrl(OAuthConfig config, Map<String, String> additionalParams){
-
-        final ParameterList parameters = new ParameterList(additionalParams);
-        parameters.add(OAuthConstants.RESPONSE_TYPE, "code");
-        parameters.add(OAuthConstants.CLIENT_ID, config.getApiKey());
-
-        final String callback = config.getCallback();
-        if (callback != null) {
-            parameters.add(OAuthConstants.REDIRECT_URI, callback);
-        }
-
-        final String scope = config.getScope();
-        if (scope != null) {
-            parameters.add(OAuthConstants.SCOPE, scope);
-        }
-
-        final String state = config.getState();
-        if (state != null) {
-            parameters.add(OAuthConstants.STATE, state);
-        }
-
-        return parameters.appendTo("https://auth.doximity.com/oauth/authorize?");
+        return "https://auth.doximity.com/oauth/authorize";
     }
 
     @Override
--- scribejava-apis/src/main/java/com/github/scribejava/apis/service/Doximity20ServiceImpl.java
+++ scribejava-apis/src/main/java/com/github/scribejava/apis/service/Doximity20ServiceImpl.java
@@ -4,7 +4,6 @@ import com.github.scribejava.core.builder.api.DefaultApi20;
 import com.github.scribejava.core.model.AbstractRequest;
 import com.github.scribejava.core.model.OAuth2AccessToken;
 import com.github.scribejava.core.model.OAuthConfig;
-import com.github.scribejava.core.model.OAuthConstants;
 import com.github.scribejava.core.oauth.OAuth20Service;
 
 public class Doximity20ServiceImpl extends OAuth20Service {
@@ -18,13 +17,6 @@ public class Doximity20ServiceImpl extends OAuth20Service {
      */
     public Doximity20ServiceImpl(DefaultApi20 api, OAuthConfig config) {
         super(api, config);
-    }
-
-    @Override
-    protected <T extends AbstractRequest> T createAccessTokenRequest(String oauthVerrifier, T request) {
-        super.createAccessTokenRequest(oauthVerrifier, request);
-        request.addParameter(OAuthConstants.GRANT_TYPE, "authorization_code");
-        return request;
     }
 
     @Override
