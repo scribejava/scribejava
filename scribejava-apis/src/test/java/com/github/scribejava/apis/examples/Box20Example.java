a47689cebdc97795d68b7380bbbcc2c5e772a63d
MclaughlinSteve <mclaughlinsteve90@gmail.com>
2016-11-02 23:26:54 -0400

    Fixed test
    Updated test. Should be working now

--- scribejava-apis/src/test/java/com/github/scribejava/apis/examples/Box20Example.java
+++ scribejava-apis/src/test/java/com/github/scribejava/apis/examples/Box20Example.java
@@ -16,7 +16,7 @@ import java.util.Map;
 public final class Box20Example {
 
     private static final String NETWORK_NAME = "Box";
-    private static final String PROTECTED_RESOURCE_URL = "https://www.googleapis.com/plus/v1/people/me";
+    private static final String PROTECTED_RESOURCE_URL = "https://app.box.com/files";
 
     private Box20Example() {
     }
@@ -25,13 +25,12 @@ public final class Box20Example {
         //Replace these with your client id and secret
         final String clientId = "your client id";
         final String clientSecret = "your client secret";
-        final String secretState = "secret" + new Random().nextInt(999_999);
+        final String secretState = "security_token" + new Random().nextInt(999_999);
         final OAuth20Service service = new ServiceBuilder()
                 .apiKey(clientId)
                 .apiSecret(clientSecret)
-                .scope("profile") // replace with desired scope
                 .state(secretState)
-                .callback("http://example.com/callback")
+                .callback("https://example.com/callback")
                 .build(BoxApi20.instance());
         final Scanner in = new Scanner(System.in, "UTF-8");
 
@@ -41,7 +40,6 @@ public final class Box20Example {
         // Obtain the Authorization URL
         System.out.println("Fetching the Authorization URL...");
         //pass access_type=offline to get refresh token
-        //https://developers.google.com/identity/protocols/OAuth2WebServer#preparing-to-start-the-oauth-20-flow
         final Map<String, String> additionalParams = new HashMap<>();
         additionalParams.put("access_type", "offline");
         //force to reget refresh token (if usera are asked not the first time)
@@ -71,42 +69,10 @@ public final class Box20Example {
         System.out.println("Trading the Request Token for an Access Token...");
         OAuth2AccessToken accessToken = service.getAccessToken(code);
         System.out.println("Got the Access Token!");
-        System.out.println("(if your curious it looks like this: " + accessToken
+        System.out.println("(If you're curious, it looks like this: " + accessToken
                 + ", 'rawResponse'='" + accessToken.getRawResponse() + "')");
 
-        System.out.println("Refreshing the Access Token...");
-        accessToken = service.refreshAccessToken(accessToken.getRefreshToken());
-        System.out.println("Refreshed the Access Token!");
-        System.out.println("(if your curious it looks like this: " + accessToken
-                + ", 'rawResponse'='" + accessToken.getRawResponse() + "')");
-        System.out.println();
-
-        // Now let's go and ask for a protected resource!
-        System.out.println("Now we're going to access a protected resource...");
-        while (true) {
-            System.out.println("Paste fieldnames to fetch (leave empty to get profile, 'exit' to stop example)");
-            System.out.print(">>");
-            final String query = in.nextLine();
-            System.out.println();
-
-            final String requestUrl;
-            if ("exit".equals(query)) {
-                break;
-            } else if (query == null || query.isEmpty()) {
-                requestUrl = PROTECTED_RESOURCE_URL;
-            } else {
-                requestUrl = PROTECTED_RESOURCE_URL + "?fields=" + query;
-            }
-
-            final OAuthRequest request = new OAuthRequest(Verb.GET, requestUrl, service);
-            service.signRequest(accessToken, request);
-            final Response response = request.send();
-            System.out.println();
-            System.out.println(response.getCode());
-            System.out.println(response.getBody());
-
-            System.out.println();
-        }
+        System.out.println("Thats it man! Go and build something awesome with ScribeJava! :)");
     }
 
 }
