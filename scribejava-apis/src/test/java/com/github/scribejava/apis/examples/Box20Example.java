6405e8b2b556dc9bd615b5095d8a1190e8e113d1
MclaughlinSteve <mclaughlinsteve90@gmail.com>
2016-11-02 23:43:35 -0400

    Updated test
    Made fixes to test

--- scribejava-apis/src/test/java/com/github/scribejava/apis/examples/Box20Example.java
+++ scribejava-apis/src/test/java/com/github/scribejava/apis/examples/Box20Example.java
@@ -16,7 +16,7 @@ import java.util.Map;
 public final class Box20Example {
 
     private static final String NETWORK_NAME = "Box";
-    private static final String PROTECTED_RESOURCE_URL = "https://app.box.com/files";
+    private static final String PROTECTED_RESOURCE_URL = "https://app.box.com/profile";
 
     private Box20Example() {
     }
@@ -67,10 +67,22 @@ public final class Box20Example {
 
         // Trade the Request Token and Verfier for the Access Token
         System.out.println("Trading the Request Token for an Access Token...");
-        OAuth2AccessToken accessToken = service.getAccessToken(code);
+        final OAuth2AccessToken accessToken = service.getAccessToken(code);
         System.out.println("Got the Access Token!");
         System.out.println("(If you're curious, it looks like this: " + accessToken
                 + ", 'rawResponse'='" + accessToken.getRawResponse() + "')");
+
+        System.out.println();
+
+        // Now let's go and ask for a protected resource!
+        System.out.println("Now we're going to access a protected resource...");
+        final OAuthRequest request = new OAuthRequest(Verb.GET, PROTECTED_RESOURCE_URL, service);
+        service.signRequest(accessToken, request);
+        final Response response = request.send();
+        System.out.println("Got it! Lets see what we found...");
+        System.out.println();
+        System.out.println(response.getCode());
+        System.out.println(response.getBody());
 
         System.out.println("Thats it man! Go and build something awesome with ScribeJava! :)");
     }
