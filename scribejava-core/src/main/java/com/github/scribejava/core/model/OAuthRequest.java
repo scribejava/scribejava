a1cb90a9688c6d36ef81ec9c4ea53b9e7db90dff
Stas Gromov <kullfar@gmail.com>
2016-12-26 18:18:59 +0300

    prepare to move HTTP communications logic from OAuthRequest to separate package

--- scribejava-core/src/main/java/com/github/scribejava/core/model/OAuthRequest.java
+++ scribejava-core/src/main/java/com/github/scribejava/core/model/OAuthRequest.java
@@ -2,19 +2,17 @@ package com.github.scribejava.core.model;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.Map;
 import com.github.scribejava.core.exceptions.OAuthConnectionException;
 import com.github.scribejava.core.exceptions.OAuthException;
 import com.github.scribejava.core.oauth.OAuthService;
 import java.io.File;
+import java.net.URL;
 import java.net.UnknownHostException;
 import java.util.HashMap;
 import java.util.List;
 
 public class OAuthRequest extends AbstractRequest {
-
-    private HttpURLConnection connection;
 
     private final OAuthConfig config;
 
@@ -34,31 +32,32 @@ public class OAuthRequest extends AbstractRequest {
      */
     public Response send() {
         try {
-            createConnection();
-            return doSend();
+            return doSend(config, isFollowRedirects(), getHeaders(), getVerb(), getCompleteUrl(), this);
         } catch (IOException | RuntimeException e) {
             throw new OAuthConnectionException(getCompleteUrl(), e);
         }
     }
 
-    Response doSend() throws IOException {
-        final Verb verb = getVerb();
-        connection.setRequestMethod(verb.name());
+    private static Response doSend(OAuthConfig config, boolean followRedirects, Map<String, String> headers,
+            Verb httpVerb, String completeUrl, OAuthRequest request) throws IOException {
+        final HttpURLConnection connection = (HttpURLConnection) new URL(completeUrl).openConnection();
+        connection.setInstanceFollowRedirects(followRedirects);
+        connection.setRequestMethod(httpVerb.name());
         if (config.getConnectTimeout() != null) {
             connection.setConnectTimeout(config.getConnectTimeout());
         }
         if (config.getReadTimeout() != null) {
             connection.setReadTimeout(config.getReadTimeout());
         }
-        addHeaders();
-        if (hasBodyContent()) {
-            final File filePayload = getFilePayload();
+        addHeaders(connection, headers, config.getUserAgent());
+        if (httpVerb == Verb.POST || httpVerb == Verb.PUT || httpVerb == Verb.DELETE) {
+            final File filePayload = request.getFilePayload();
             if (filePayload != null) {
                 throw new UnsupportedOperationException("Sync Requests do not support File payload for the moment");
-            } else if (getStringPayload() != null) {
-                addBody(getStringPayload().getBytes(getCharset()));
+            } else if (request.getStringPayload() != null) {
+                addBody(connection, request.getStringPayload().getBytes(request.getCharset()));
             } else {
-                addBody(getByteArrayPayload());
+                addBody(connection, request.getByteArrayPayload());
             }
         }
 
@@ -86,25 +85,16 @@ public class OAuthRequest extends AbstractRequest {
         return headers;
     }
 
-    private void createConnection() throws IOException {
-        final String completeUrl = getCompleteUrl();
-        if (connection == null) {
-            connection = (HttpURLConnection) new URL(completeUrl).openConnection();
-            connection.setInstanceFollowRedirects(isFollowRedirects());
-        }
-    }
-
-    void addHeaders() {
-        for (Map.Entry<String, String> entry : getHeaders().entrySet()) {
+    private static void addHeaders(HttpURLConnection connection, Map<String, String> headers, String userAgent) {
+        for (Map.Entry<String, String> entry : headers.entrySet()) {
             connection.setRequestProperty(entry.getKey(), entry.getValue());
         }
-        final String userAgent = config.getUserAgent();
         if (userAgent != null) {
             connection.setRequestProperty(OAuthConstants.USER_AGENT_HEADER_NAME, userAgent);
         }
     }
 
-    void addBody(byte[] content) throws IOException {
+    private static void addBody(HttpURLConnection connection, byte[] content) throws IOException {
         connection.setRequestProperty(CONTENT_LENGTH, String.valueOf(content.length));
 
         if (connection.getRequestProperty(CONTENT_TYPE) == null) {
@@ -112,9 +102,5 @@ public class OAuthRequest extends AbstractRequest {
         }
         connection.setDoOutput(true);
         connection.getOutputStream().write(content);
-    }
-
-    void setConnection(HttpURLConnection connection) {
-        this.connection = connection;
     }
 }
--- scribejava-core/src/test/java/com/github/scribejava/core/model/RequestTest.java
+++ scribejava-core/src/test/java/com/github/scribejava/core/model/RequestTest.java
@@ -1,7 +1,6 @@
 package com.github.scribejava.core.model;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import org.junit.Before;
 import org.junit.Test;
@@ -11,26 +10,18 @@ public class RequestTest {
 
     private OAuthRequest getRequest;
     private OAuthRequest postRequest;
-    private ConnectionStub connection;
     private OAuthConfig config;
 
     @Before
     public void setUp() throws MalformedURLException {
-        connection = new ConnectionStub();
         config = new OAuthConfig("test", "test");
+
         postRequest = new OAuthRequest(Verb.POST, "http://example.com", config);
         postRequest.addBodyParameter("param", "value");
         postRequest.addBodyParameter("param with spaces", "value with spaces");
-        postRequest.setConnection(connection);
+
         getRequest = new OAuthRequest(Verb.GET, "http://example.com?qsparam=value&other+param=value+with+spaces",
                 config);
-        getRequest.setConnection(connection);
-    }
-
-    @Test
-    public void shouldSetRequestVerb() {
-        getRequest.send();
-        assertEquals("GET", connection.getRequestMethod());
     }
 
     @Test
@@ -41,28 +32,15 @@ public class RequestTest {
     }
 
     @Test
-    public void shouldAddRequestHeaders() {
-        getRequest.addHeader("Header", "1");
-        getRequest.addHeader("Header2", "2");
-        getRequest.send();
-        assertEquals(2, getRequest.getHeaders().size());
-        assertEquals(2, connection.getHeaders().size());
-    }
-
-    @Test
     public void shouldSetBodyParamsAndAddContentLength() {
         assertEquals("param=value&param%20with%20spaces=value%20with%20spaces",
                 new String(postRequest.getByteArrayPayload()));
-        postRequest.send();
-        assertTrue(connection.getHeaders().containsKey("Content-Length"));
     }
 
     @Test
     public void shouldSetPayloadAndHeaders() {
         postRequest.setPayload("PAYLOAD");
-        postRequest.send();
         assertEquals("PAYLOAD", postRequest.getStringPayload());
-        assertTrue(connection.getHeaders().containsKey("Content-Length"));
     }
 
     @Test
@@ -84,38 +62,5 @@ public class RequestTest {
     @Test
     public void shouldHandleQueryStringSpaceEncodingProperly() {
         assertTrue(getRequest.getQueryStringParams().contains(new Parameter("other param", "value with spaces")));
-    }
-
-    @Test
-    public void shouldAutomaticallyAddContentTypeForPostRequestsWithBytePayload() {
-        postRequest.setPayload("PAYLOAD".getBytes());
-        postRequest.send();
-        assertEquals(OAuthRequest.DEFAULT_CONTENT_TYPE, connection.getHeaders().get("Content-Type"));
-    }
-
-    @Test
-    public void shouldAutomaticallyAddContentTypeForPostRequestsWithStringPayload() {
-        postRequest.setPayload("PAYLOAD");
-        postRequest.send();
-        assertEquals(OAuthRequest.DEFAULT_CONTENT_TYPE, connection.getHeaders().get("Content-Type"));
-    }
-
-    @Test
-    public void shouldAutomaticallyAddContentTypeForPostRequestsWithBodyParameters() {
-        postRequest.send();
-        assertEquals(OAuthRequest.DEFAULT_CONTENT_TYPE, connection.getHeaders().get("Content-Type"));
-    }
-
-    @Test
-    public void shouldBeAbleToOverrideItsContentType() {
-        postRequest.addHeader("Content-Type", "my-content-type");
-        postRequest.send();
-        assertEquals("my-content-type", connection.getHeaders().get("Content-Type"));
-    }
-
-    @Test
-    public void shouldNotAddContentTypeForGetRequests() {
-        getRequest.send();
-        assertFalse(connection.getHeaders().containsKey("Content-Type"));
     }
 }
