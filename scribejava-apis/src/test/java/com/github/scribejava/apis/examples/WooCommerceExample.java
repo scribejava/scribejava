b579853a05c5d8c414f97ff1eeaa9eb08506d0ce
Luigi Ruocco <luigi.ruocco@entaksi.eu>
2017-05-05 23:34:13 +0200

    Added WooCommerce Example

---
+++ scribejava-apis/src/test/java/com/github/scribejava/apis/examples/WooCommerceExample.java
@@ -0,0 +1,51 @@
+package com.github.scribejava.apis.examples;
+
+import com.github.scribejava.apis.WooCommerceApi;
+import com.github.scribejava.core.builder.ServiceBuilder;
+import com.github.scribejava.core.model.OAuth1AccessToken;
+import com.github.scribejava.core.model.OAuthRequest;
+import com.github.scribejava.core.model.Response;
+import com.github.scribejava.core.model.Verb;
+import com.github.scribejava.core.oauth.OAuth10aService;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+
+public final class WooCommerceExample
+{
+
+    private static final String NETWORK_NAME = "WooCommerce";
+    private static final String PROTECTED_RESOURCE_URL = "http://your-site/wordpress/wp-json/wc/v2/products";
+
+    private WooCommerceExample()
+    {
+    }
+
+    public static void main(String... args) throws IOException, InterruptedException, ExecutionException
+    {
+        // Replace these with your client id and secret
+        final String apiKey = "your_app_id";
+        final String apiSecret = "your_api_secret";
+
+        OAuth10aService service = new ServiceBuilder()
+                .apiKey(apiKey)
+                .apiSecret(apiSecret)
+                .debugStream(System.out)
+                .build(WooCommerceApi.instance());
+
+        System.out.println("=== " + NETWORK_NAME + "'s OAuth Workflow ===");
+        System.out.println();
+
+        System.out.println("Now we're going to access a protected resource...");
+        final OAuthRequest request = new OAuthRequest(Verb.GET, PROTECTED_RESOURCE_URL);
+        service.signRequest(new OAuth1AccessToken("", ""), request);
+        final Response response = service.execute(request);
+        System.out.println("Got it! Lets see what we found...");
+        System.out.println();
+        System.out.println(response.getCode());
+        System.out.println(response.getBody());
+
+        System.out.println();
+        System.out.println("Thats it man! Go and build something awesome with ScribeJava! :)");
+    }
+}
