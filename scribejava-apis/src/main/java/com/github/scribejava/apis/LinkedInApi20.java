30824b340cc68c6b5670450e3c295a3a108a76ee
Stas Gromov <kullfar@gmail.com>
2017-05-29 17:21:51 +0300

    LinkedIn use Header to sign OAuth2 requests

--- scribejava-apis/src/main/java/com/github/scribejava/apis/LinkedInApi20.java
+++ scribejava-apis/src/main/java/com/github/scribejava/apis/LinkedInApi20.java
@@ -1,10 +1,6 @@
 package com.github.scribejava.apis;
 
-import com.github.scribejava.apis.service.LinkedIn20ServiceImpl;
 import com.github.scribejava.core.builder.api.DefaultApi20;
-import com.github.scribejava.core.builder.api.OAuth2SignatureType;
-import com.github.scribejava.core.model.OAuthConfig;
-import com.github.scribejava.core.oauth.OAuth20Service;
 
 public class LinkedInApi20 extends DefaultApi20 {
 
@@ -27,15 +23,5 @@ public class LinkedInApi20 extends DefaultApi20 {
     @Override
     protected String getAuthorizationBaseUrl() {
         return "https://www.linkedin.com/oauth/v2/authorization";
-    }
-
-    @Override
-    public OAuth20Service createService(OAuthConfig config) {
-        return new LinkedIn20ServiceImpl(this, config);
-    }
-
-    @Override
-    public OAuth2SignatureType getSignatureType() {
-        return OAuth2SignatureType.BEARER_URI_QUERY_PARAMETER;
     }
 }
