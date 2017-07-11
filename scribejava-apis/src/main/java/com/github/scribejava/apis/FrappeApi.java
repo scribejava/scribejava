5c2c48b925bb0bc0f45f5905fc68e0cc1305e631
Revant Nandgaonkar <revant.one@gmail.com>
2017-07-11 20:42:56 +0530

    Added Frappe.io OAuth2 API

---
+++ scribejava-apis/src/main/java/com/github/scribejava/apis/FrappeApi.java
@@ -0,0 +1,58 @@
+package com.github.scribejava.apis;
+
+import com.github.scribejava.core.builder.api.DefaultApi20;
+import com.github.scribejava.core.exceptions.OAuthException;
+import com.github.scribejava.core.extractors.TokenExtractor;
+import com.github.scribejava.core.model.OAuth2AccessToken;
+import com.github.scribejava.core.model.Response;
+import com.github.scribejava.core.model.Verb;
+
+import java.io.IOException;
+
+public class FrappeApi extends DefaultApi20 {
+
+    String SERVER_URL = "https://erpnext.org";
+
+    protected FrappeApi() {
+    }
+
+    private static class InstanceHolder {
+        private static final FrappeApi INSTANCE = new FrappeApi();
+    }
+
+    public static FrappeApi instance() {
+        return InstanceHolder.INSTANCE;
+    }
+
+    @Override
+    public Verb getAccessTokenVerb() {
+        return Verb.POST;
+    }
+
+    @Override
+    public String getAccessTokenEndpoint() {
+        return this.SERVER_URL + "/api/method/frappe.integrations.oauth2.get_token";
+    }
+
+    @Override
+    protected String getAuthorizationBaseUrl() {
+        return this.SERVER_URL + "/api/method/frappe.integrations.oauth2.authorize";
+    }
+
+    @Override
+    public TokenExtractor<OAuth2AccessToken> getAccessTokenExtractor() {
+        return new TokenExtractor<OAuth2AccessToken>() {
+            @Override
+            public OAuth2AccessToken extract(Response response) throws IOException, OAuthException {
+                String token = null;
+                try {
+                    JSONObject bearerToken = new JSONObject(response.getBody());
+                    token = bearerToken.getString("access_token");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                return new OAuth2AccessToken(token);
+            }
+        };
+    }
+}
---
+++ scribejava-apis/src/main/java/com/github/scribejava/apis/frappe/FrappeToken.java
@@ -0,0 +1,70 @@
+package com.github.scribejava.apis.frappe;
+
+import com.github.scribejava.core.model.OAuth2AccessToken;
+import java.util.Objects;
+
+public class FrappeToken extends OAuth2AccessToken {
+
+    private static final long serialVersionUID = 7845679917727899612L;
+
+    /**
+     * Id_token is part of OpenID Connect specification. It can hold user information that you can directly extract
+     * without additional request to provider.
+     *
+     * See http://openid.net/specs/openid-connect-core-1_0.html#id_token-tokenExample and
+     * https://bitbucket.org/nimbusds/nimbus-jose-jwt/wiki/Home
+     *
+     * Here will be encoded and signed id token in JWT format or null, if not defined.
+     */
+    private final String openIdToken;
+
+    public FrappeToken(String accessToken, String openIdToken, String rawResponse) {
+        this(accessToken, null, null, null, null, openIdToken, rawResponse);
+    }
+
+    public FrappeToken(String accessToken, String tokenType, Integer expiresIn, String refreshToken, String scope,
+            String openIdToken, String rawResponse) {
+        super(accessToken, tokenType, expiresIn, refreshToken, scope, rawResponse);
+        this.openIdToken = openIdToken;
+    }
+
+    public String getOpenIdToken() {
+        return openIdToken;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = super.hashCode();
+        hash = 37 * hash + Objects.hashCode(openIdToken);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        if (!super.equals(obj)) {
+            return false;
+        }
+
+        return Objects.equals(openIdToken, ((FrappeToken) obj).getOpenIdToken());
+    }
+
+    @Override
+    public String toString() {
+        return "FrappeToken{"
+                + "access_token=" + getAccessToken()
+                + ", token_type=" + getTokenType()
+                + ", expires_in=" + getExpiresIn()
+                + ", refresh_token=" + getRefreshToken()
+                + ", scope=" + getScope()
+                + ", open_id_token=" + openIdToken + '}';
+    }
+}
