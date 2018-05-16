8b8d7c0aa48728d3eb1f5b96d1f92665479b671f
Dan Manastireanu <dan.manastireanu@endava.com>
2018-05-16 17:14:37 +0300

    Fix Fitbit token error extraction

--- scribejava-apis/src/main/java/com/github/scribejava/apis/fitbit/FitBitJsonTokenExtractor.java
+++ scribejava-apis/src/main/java/com/github/scribejava/apis/fitbit/FitBitJsonTokenExtractor.java
@@ -1,12 +1,16 @@
 package com.github.scribejava.apis.fitbit;
 
 import com.github.scribejava.core.extractors.OAuth2AccessTokenJsonExtractor;
+import com.github.scribejava.core.model.OAuth2AccessTokenErrorResponse;
+
+import java.net.URI;
 import java.util.regex.Pattern;
 
 public class FitBitJsonTokenExtractor extends OAuth2AccessTokenJsonExtractor {
 
     private static final Pattern USER_ID_REGEX_PATTERN = Pattern.compile("\"user_id\"\\s*:\\s*\"(\\S*?)\"");
-
+    private static final Pattern ERROR_REGEX_PATTERN = Pattern.compile("\"errorType\"\\s*:\\s*\"(\\S*?)\"");
+    private static final Pattern ERROR_DESCRIPTION_REGEX_PATTERN = Pattern.compile("\"message\"\\s*:\\s*\"([^\"]*?)\"");
     protected FitBitJsonTokenExtractor() {
     }
 
@@ -24,5 +28,22 @@ public class FitBitJsonTokenExtractor extends OAuth2AccessTokenJsonExtractor {
             String refreshToken, String scope, String response) {
         return new FitBitOAuth2AccessToken(accessToken, tokenType, expiresIn, refreshToken, scope,
                 extractParameter(response, USER_ID_REGEX_PATTERN, false), response);
+    }
+
+    @Override
+    public void generateError(String response) {
+        final String errorInString = extractParameter(response, ERROR_REGEX_PATTERN, true);
+        final String errorDescription = extractParameter(response, ERROR_DESCRIPTION_REGEX_PATTERN, false);
+        final URI errorUri = URI.create("https://dev.fitbit.com/build/reference/web-api/oauth2/");
+
+        OAuth2AccessTokenErrorResponse.ErrorCode errorCode;
+        try {
+            errorCode = OAuth2AccessTokenErrorResponse.ErrorCode.valueOf(errorInString);
+        } catch (IllegalArgumentException iaE) {
+            //non oauth standard error code
+            errorCode = null;
+        }
+
+        throw new OAuth2AccessTokenErrorResponse(errorCode, errorDescription, errorUri, response);
     }
 }
