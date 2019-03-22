b9ad89463744408ca451849bc4787400dce54c1b
Stas Gromov <kullfar@gmail.com>
2019-03-22 18:10:18 +0300

    upgrade VkontakteApi from 5.73 to 5.92

--- scribejava-apis/src/main/java/com/github/scribejava/apis/VkontakteApi.java
+++ scribejava-apis/src/main/java/com/github/scribejava/apis/VkontakteApi.java
@@ -12,7 +12,7 @@ import com.github.scribejava.core.oauth2.clientauthentication.RequestBodyAuthent
 
 public class VkontakteApi extends DefaultApi20 {
 
-    public static final String VERSION = "5.73";
+    public static final String VERSION = "5.92";
 
     protected VkontakteApi() {
     }
