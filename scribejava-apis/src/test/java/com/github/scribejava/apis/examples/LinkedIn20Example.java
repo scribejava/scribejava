4f84b315f17d4a4623ed78cf073b7dc8b2792865
Stas Gromov <kullfar@gmail.com>
2019-05-20 14:22:28 +0300

    Update LinkedIn Example to API v2 (thanks to https://github.com/peternees)

--- scribejava-apis/src/test/java/com/github/scribejava/apis/examples/LinkedIn20Example.java
+++ scribejava-apis/src/test/java/com/github/scribejava/apis/examples/LinkedIn20Example.java
@@ -9,12 +9,15 @@ import com.github.scribejava.core.model.Response;
 import com.github.scribejava.core.model.Verb;
 import com.github.scribejava.core.oauth.OAuth20Service;
 import java.io.IOException;
+import java.util.Random;
 import java.util.concurrent.ExecutionException;
 
 public class LinkedIn20Example {
 
     private static final String NETWORK_NAME = "LinkedIn";
-    private static final String PROTECTED_RESOURCE_URL = "https://api.linkedin.com/v1/people/~:(%s)";
+    private static final String PROTECTED_RESOURCE_URL = "https://api.linkedin.com/v2/me";
+    private static final String PROTECTED_EMAIL_RESOURCE_URL
+            = "https://api.linkedin.com/v2/emailAddress?q=members&projection=(elements*(handle~))";
 
     private LinkedIn20Example() {
     }
@@ -26,7 +29,7 @@ public class LinkedIn20Example {
         final String clientSecret = "your client secret";
         final OAuth20Service service = new ServiceBuilder(clientId)
                 .apiSecret(clientSecret)
-                .defaultScope("r_basicprofile r_emailaddress") // replace with desired scope
+                .defaultScope("r_liteprofile r_emailaddress") // replace with desired scope
                 .callback("http://example.com/callback")
                 .build(LinkedInApi20.instance());
         final Scanner in = new Scanner(System.in);
@@ -36,7 +39,8 @@ public class LinkedIn20Example {
 
         // Obtain the Authorization URL
         System.out.println("Fetching the Authorization URL...");
-        final String authorizationUrl = service.getAuthorizationUrl("some_params");
+        final String secretState = "secret" + new Random().nextInt(999_999);
+        final String authorizationUrl = service.getAuthorizationUrl(secretState);
         System.out.println("Got the Authorization URL!");
         System.out.println("Now go and authorize ScribeJava here:");
         System.out.println(authorizationUrl);
@@ -52,28 +56,29 @@ public class LinkedIn20Example {
         System.out.println("(The raw response looks like this: " + accessToken.getRawResponse() + "')");
         System.out.println();
 
-        // Now let's go and ask for a protected resource!
-        System.out.println("Now we're going to access a protected resource...");
-        while (true) {
-            System.out.println("Paste profile query for fetch (firstName, lastName, etc) or 'exit' to stop example");
-            System.out.print(">>");
-            final String query = in.nextLine();
-            System.out.println();
+        System.out.println("Now we're going to get the email of the current user...");
+        final OAuthRequest emailRequest = new OAuthRequest(Verb.GET, PROTECTED_EMAIL_RESOURCE_URL);
+        emailRequest.addHeader("x-li-format", "json");
+        emailRequest.addHeader("Accept-Language", "ru-RU");
+        service.signRequest(accessToken, emailRequest);
+        final Response emailResponse = service.execute(emailRequest);
+        System.out.println();
+        System.out.println(emailResponse.getCode());
+        System.out.println(emailResponse.getBody());
 
-            if ("exit".equals(query)) {
-                break;
-            }
+        System.out.println();
 
-            final OAuthRequest request = new OAuthRequest(Verb.GET, String.format(PROTECTED_RESOURCE_URL, query));
-            request.addHeader("x-li-format", "json");
-            request.addHeader("Accept-Language", "ru-RU");
-            service.signRequest(accessToken, request);
-            final Response response = service.execute(request);
-            System.out.println();
-            System.out.println(response.getCode());
-            System.out.println(response.getBody());
+        System.out.println("Now we're going to access a protected profile resource...");
 
-            System.out.println();
-        }
+        final OAuthRequest request = new OAuthRequest(Verb.GET, PROTECTED_RESOURCE_URL);
+        request.addHeader("x-li-format", "json");
+        request.addHeader("Accept-Language", "ru-RU");
+        service.signRequest(accessToken, request);
+        final Response response = service.execute(request);
+        System.out.println();
+        System.out.println(response.getCode());
+        System.out.println(response.getBody());
+
+        System.out.println();
     }
 }
