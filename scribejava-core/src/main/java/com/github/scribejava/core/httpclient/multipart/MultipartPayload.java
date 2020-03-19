a7b01f632ac4283db7b7833d64b17a717de4bee7
Stas Gromov <kullfar@gmail.com>
2020-03-19 18:39:32 +0300

    update deps

--- scribejava-core/src/main/java/com/github/scribejava/core/httpclient/multipart/MultipartPayload.java
+++ scribejava-core/src/main/java/com/github/scribejava/core/httpclient/multipart/MultipartPayload.java
@@ -77,7 +77,7 @@ public class MultipartPayload extends BodyPartPayload {
 
     static void checkBoundarySyntax(String boundary) {
         if (boundary == null || !BOUNDARY_REGEXP.matcher(boundary).matches()) {
-            throw new IllegalArgumentException("{'boundary'='" + boundary + "'} has invaid syntax. Should be '"
+            throw new IllegalArgumentException("{'boundary'='" + boundary + "'} has invalid syntax. Should be '"
                     + BOUNDARY_PATTERN + "'.");
         }
     }
