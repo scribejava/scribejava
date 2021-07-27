30da5f4d1b629273a41093d6086969df2f33199d
Eugeny Zibrov <e.zibrov@hh.ru>
2021-07-27 21:53:25 +0300

    Update endpoints for Doktornarabote

--- scribejava-apis/src/main/java/com/github/scribejava/apis/DoktornaraboteApi.java
+++ scribejava-apis/src/main/java/com/github/scribejava/apis/DoktornaraboteApi.java
@@ -1,8 +1,16 @@
 package com.github.scribejava.apis;
 
 import com.github.scribejava.core.builder.api.DefaultApi20;
+import com.github.scribejava.core.model.OAuthConstants;
+import com.github.scribejava.core.model.ParameterList;
+import com.github.scribejava.core.model.Verb;
+import com.github.scribejava.core.oauth2.clientauthentication.ClientAuthentication;
+import com.github.scribejava.core.oauth2.clientauthentication.RequestBodyAuthenticationScheme;
+import java.util.Map;
 
 public class DoktornaraboteApi extends DefaultApi20 {
+
+    private static final String REDIRECT_PARAM_NAME = "redirect";
 
     protected DoktornaraboteApi() {
     }
@@ -17,11 +25,44 @@ public class DoktornaraboteApi extends DefaultApi20 {
 
     @Override
     public String getAccessTokenEndpoint() {
-        return "https://auth.doktornarabote.ru/OAuth/Token";
+        return "https://id.doktornarabote.ru/api/v1/auth/token";
+    }
+
+    @Override
+    public Verb getAccessTokenVerb() {
+        return Verb.GET;
     }
 
     @Override
     protected String getAuthorizationBaseUrl() {
-        return "https://auth.doktornarabote.ru/OAuth/Authorize";
+        return "https://www.doktornarabote.ru";
     }
+
+    @Override
+    public ClientAuthentication getClientAuthentication() {
+        return RequestBodyAuthenticationScheme.instance();
+    }
+
+    @Override
+    public String getAuthorizationUrl(String responseType, String apiKey, String callback, String scope, String state,
+                                      Map<String, String> additionalParams) {
+        final ParameterList parameters = new ParameterList(additionalParams);
+        parameters.add(OAuthConstants.RESPONSE_TYPE, responseType);
+        parameters.add(OAuthConstants.CLIENT_ID, apiKey);
+
+        if (callback != null) {
+            parameters.add(REDIRECT_PARAM_NAME, callback);
+        }
+
+        if (scope != null) {
+            parameters.add(OAuthConstants.SCOPE, scope);
+        }
+
+        if (state != null) {
+            parameters.add(OAuthConstants.STATE, state);
+        }
+
+        return parameters.appendTo(getAuthorizationBaseUrl());
+    }
+
 }
