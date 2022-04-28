550aa12bebac7a9384004a741d62883b5965163f
Peter Phillpotts <peter.p@canva.com>
2022-04-28 15:17:06 +1000

    SlackJsonTokenExtractor handle null access_token
    Issue: https://github.com/scribejava/scribejava/issues/1023
    Slack does not always provide the bot access token, if you don't ask for those scopes.
    This 
    
    Note - the underlying OAuth2AccessToken has a Precondition for "access_token can't be null", so here it is provided with an empty string.

--- scribejava-apis/src/main/java/com/github/scribejava/apis/slack/SlackJsonTokenExtractor.java
+++ scribejava-apis/src/main/java/com/github/scribejava/apis/slack/SlackJsonTokenExtractor.java
@@ -2,6 +2,12 @@ package com.github.scribejava.apis.slack;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.github.scribejava.core.extractors.OAuth2AccessTokenJsonExtractor;
+import com.github.scribejava.core.model.OAuth2AccessToken;
+import com.github.scribejava.core.model.OAuthConstants;
+import com.github.scribejava.core.model.Response;
+import com.github.scribejava.core.utils.Preconditions;
+
+import java.io.IOException;
 
 public class SlackJsonTokenExtractor extends OAuth2AccessTokenJsonExtractor {
 
@@ -15,6 +21,37 @@ public class SlackJsonTokenExtractor extends OAuth2AccessTokenJsonExtractor {
 
     public static SlackJsonTokenExtractor instance() {
         return SlackJsonTokenExtractor.InstanceHolder.INSTANCE;
+    }
+
+    @Override
+    public OAuth2AccessToken extract(Response response) throws IOException {
+        final String body = response.getBody();
+        Preconditions.checkEmptyString(body,
+                "Response body is incorrect. Can't extract a token from an empty string");
+
+        if (response.getCode() != 200) {
+            generateError(response);
+        }
+        return createToken(body);
+    }
+
+    // Overridden private method
+    private OAuth2AccessToken createToken(String rawResponse) throws IOException {
+
+        final JsonNode response = OBJECT_MAPPER.readTree(rawResponse);
+
+        final JsonNode expiresInNode = response.get("expires_in");
+        final JsonNode refreshToken = response.get(OAuthConstants.REFRESH_TOKEN);
+        final JsonNode scope = response.get(OAuthConstants.SCOPE);
+        final JsonNode tokenType = response.get("token_type");
+        final JsonNode globalAccessToken = response.get(OAuthConstants.ACCESS_TOKEN);
+
+        return createToken( //
+                globalAccessToken == null ? "" : globalAccessToken.asText(), // Avoid "access_token can't be null"
+                tokenType == null ? null : tokenType.asText(),
+                expiresInNode == null ? null : expiresInNode.asInt(),
+                refreshToken == null ? null : refreshToken.asText(),
+                scope == null ? null : scope.asText(), response, rawResponse);
     }
 
     @Override
